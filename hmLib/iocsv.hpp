6d0f263a4a281f86b9bdd9e93ffa0cfb3180753f
hmito <hmito@outlook.com>
2015-07-18 22:18:31 +0900

    Add table interface

--- hmLib/iocsv.hpp
+++ hmLib/iocsv.hpp
@@ -1,8 +1,11 @@
 ﻿#ifndef HMLIB_IOCSV_INC
 #define HMLIB_IOCSV_INC 100
 #
+#include<string>
+#include<hmLib/table.hpp>
+#include<hmLib/csv_iterator.hpp>
 namespace hmLib{
-
+	table<std::string> read_csv();
 }
 #
 #endif
--- hmLib/table.hpp
+++ hmLib/table.hpp
@@ -1,12 +1,25 @@
 ﻿#ifndef HMLIB_TABLE_INC
 #define HMLIB_TABLE_INC
 #
+#include<hmLib/position.hpp>
 namespace hmLib{
 	template<typename T>
-	class table;
+	class table{
+	public:
+		using size_type = unsigned int;
+		using column_type = std::vector < T > ;
+	private:
+	public:
+		T& at(size_type pos_row, size_type pos_col);
+		const T& at(size_type pos_row, size_type pos_col)const;
+		T& operator()(size_type pos_row, size_type pos_col);
+		const T& operator()(size_type pos_row, size_type pos_col)const;
+		size_type column_size(size_type column_pos_)const;
+	};
 
 	template<typename T>
 	class named_table;
+
 }
 #
 #endif
