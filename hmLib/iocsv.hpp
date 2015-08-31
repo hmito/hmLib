618e7732004af61ec58b8c11900e1e4983b68380
hmito <hmito@outlook.com>
2015-08-31 19:55:28 +0900

    Add read_csv/write_csv functions

--- hmLib/iocsv.hpp
+++ hmLib/iocsv.hpp
@@ -4,9 +4,15 @@
 #include<string>
 #include<hmLib/table.hpp>
 #include<hmLib/csv_iterator.hpp>
+#include<hmLib/exceptions.hpp>
 namespace hmLib{
+	namespace{
+		struct iocsv_identifier{};
+	}
+	using iocsv_exception = hmLib::exceptions::exception_pattern<iocsv_identifier>;
+
 	template<typename Elem=char, typename Traits=std::char_traits<Elem> >
-	basic_table<Elem, Traits> read_csv(std::string FileName, Elem SepChar=',', Elem EndChar='\n', bool HasNameRow = false, bool IsHorizontalColumn = false){
+	basic_table<Elem, Traits> read_csv(std::string FileName, bool HasNameRow = false, bool IsHorizontalColumn = false, Elem SepChar = ',', Elem EndChar = '\n'){
 		using my_ifstream = std::basic_ifstream<Elem, Traits>;
 		using my_table = basic_table<Elem, Traits>;
 		using my_icsv_iterator = basic_icsv_iterator<Elem, Traits>;
@@ -14,7 +20,7 @@ namespace hmLib{
 		//open CSV file, or return null data
 		my_ifstream fin(FileName);
 		if(!fin.is_open())return my_table();
-	
+
 		//get iterator
 		my_icsv_iterator beg(fin, SepChar, EndChar);
 		my_icsv_iterator end = beg.get_file_end();
@@ -22,48 +28,124 @@ namespace hmLib{
 		//count row and column
 		unsigned int RowNum = 0;
 		unsigned int ColumnNum = 0;
-		unsigned int ColumnCnt = 0;
-		for(my_icsv_iterator itr = beg; itr != end; ++itr){			
-			if(itr.eol()){
-				ColumnNum = std::max(ColumnNum, ColumnCnt);
-				ColumnCnt = 0;
-				++RowNum;
-			}else{
-				++ColumnCnt;
+
+		//investigate max size of row/column
+		{
+			unsigned int ColumnCnt = 0;
+			for(my_icsv_iterator itr = beg; itr != end; ++itr){
+				if(itr.eol()){
+					ColumnNum = std::max(ColumnNum, ColumnCnt);
+					ColumnCnt = 0;
+					++RowNum;
+				} else{
+					++ColumnCnt;
+				}
 			}
+			ColumnNum = std::max(ColumnNum, ColumnCnt);
+			++RowNum;
 		}
-		ColumnNum = std::max(ColumnNum, ColumnCnt);
-		++RowNum;
 
 		//create table
-		my_table Table(RowNum,ColumnNum);
-		unsigned int RowCnt = 0;
-		ColumnCnt = 0;
-		for(my_icsv_iterator itr = beg; itr != end;){
-			Table(RowCnt, ColumnCnt) = *itr;
-			++itr;
+		my_table Table;
 
-			if(itr.eol()){
-				ColumnCnt = 0;
-				++RowCnt;
-			}else{
-				++ColumnCnt;
+		//read Horizontal Column
+		if(IsHorizontalColumn){
+			if(HasNameRow){
+				Table.assign(ColumnNum-1, RowNum);
+			} else{
+				Table.assign(ColumnNum, RowNum);
+			}
+
+			my_icsv_iterator itr = beg;
+
+			unsigned int RowCnt = 0;
+			unsigned int ColumnCnt = 0;
+			while(itr != end){
+				if(HasNameRow){
+					if(ColumnCnt == 0) Table.column(RowCnt).rename(*itr);
+					else Table(ColumnCnt-1, RowCnt) = *itr;
+				} else{
+					Table(ColumnCnt, RowCnt) = *itr;
+				}
+				++itr;
+
+				if(itr.eol()){
+					ColumnCnt = 0;
+					++RowCnt;
+				} else{
+					++ColumnCnt;
+				}
+			}
+		} else {
+			if(HasNameRow){
+				Table.assign(RowNum - 1, ColumnNum);
+			} else{
+				Table.assign(RowNum, ColumnNum);
+			}
+
+			my_icsv_iterator itr = beg;
+
+			unsigned int RowCnt = 0;
+			unsigned int ColumnCnt = 0;
+
+			if(HasNameRow){
+				do{
+					Table.column(ColumnCnt++).rename(*itr++);
+				} while(!itr.eol());
+			}
+
+			ColumnCnt = 0;
+			while(itr != end){
+				Table(RowCnt, ColumnCnt) = *itr;
+				++itr;
+
+				if(itr.eol()){
+					ColumnCnt = 0;
+					++RowCnt;
+				} else{
+					++ColumnCnt;
+				}
 			}
 		}
 
 		return std::move(Table);
 	}
+	template<typename Elem = char, typename Traits = std::char_traits<Elem> >
+	void write_csv(const basic_table<Elem, Traits>& Table,std::string FileName, bool HasNameRow = false, bool IsHorizontalColumn = false, Elem SepChar = ',', Elem EndChar = '\n'){
+		using my_ofstream = std::basic_ofstream<Elem, Traits>;
+		using my_ocsv_iterator = basic_ocsv_iterator<Elem, Traits>;
 
-	//template<typename Elem=char, typename Traits=std::char_traits<Elem>>
-	//virtualなstringを返すtableがほしい
-	//基本的な動作は同じ　違いは読みだされるまでメモリに文字を格納しない点のみ
-	//istream_stringみたいなものを作って全部解決しないか？	
-	//	書き込まれたところだけ、stringで保持し直す
-	//	optionalが必要か eitherでも可
-	//読み書きが両方できるはず
-	//const string& へのcast operatorとstr関数を両方持つべきか
-	//empty/size関数なんかは保持していてもいいかも
-	//異なるtemplate引数に基づくbasic_table間のconvert機能も欲しい
+		//open CSV file, or return null data
+		my_ofstream fout(FileName);
+		hmLib_assert(fout.is_open(), iocsv_exception, "fail to open \"" + FileName + "\" for csv file");
+
+		//get iterator
+		my_ocsv_iterator itr(fout, SepChar, EndChar);
+
+		if(IsHorizontalColumn){
+			for(unsigned int ColumnCnt = 0; ColumnCnt < Table.column_size(); ++ColumnCnt){
+				if(HasNameRow)*itr++ = Table.column(ColumnCnt).name();
+				for(unsigned int RowCnt = 0; RowCnt < Table.row_size(); ++RowCnt){
+					*itr++ = Table.at(RowCnt, ColumnCnt);
+				}
+				itr.endl();
+			}
+		} else{
+			if(HasNameRow){
+				for(unsigned int ColumnCnt = 0; ColumnCnt < Table.column_size(); ++ColumnCnt){
+					*itr++ = Table.column(ColumnCnt).name();
+				}
+				itr.endl();
+			}
+
+			for(unsigned int RowCnt = 0; RowCnt < Table.row_size(); ++RowCnt){
+				for(unsigned int ColumnCnt = 0; ColumnCnt < Table.column_size(); ++ColumnCnt){
+					*itr++ = Table.at(RowCnt, ColumnCnt);
+				}
+				itr.endl();
+			}
+		}
+	}
 }
 #
 #endif
--- vc/hmLib/hmLib/run_csv.cpp
+++ vc/hmLib/hmLib/run_csv.cpp
@@ -8,65 +8,56 @@
 #include<hmLib/iocsv.hpp>
 
 
+void output_table(hmLib::table& Table){
+	for(unsigned int x = 0; x < Table.column_size(); ++x){
+		std::cout << "[" << Table.column(x).name() << "] ";
+		for(unsigned int y = 0; y < Table.row_size(); ++y){
+			if(y != 0)std::cout << ",";
+			std::cout<< Table.column(x).at(y);
+		}
+		std::cout << std::endl;
+	}
+}
+void output_table_citr(hmLib::table& Table){
+	for(auto itr = Table.column_begin(); itr != Table.column_end(); ++itr){
+		std::cout << "[" << itr->name() << "] ";
+		for(auto iitr = itr->begin(); iitr != itr->end(); ++iitr){
+			if(iitr != itr->begin())std::cout << ",";
+			std::cout << *iitr;
+		}
+		std::cout << std::endl;
+	}
+}
+void output_table_ritr(hmLib::table& Table){
+	for(auto itr = Table.column_name_begin(); itr != Table.column_name_end(); ++itr){
+		if(itr != Table.column_name_begin())std::cout << ",";
+		std::cout << *itr;
+	}
+	std::cout << std::endl;
+
+	for(auto itr = Table.row_begin(); itr != Table.row_end(); ++itr){
+		for(auto iitr = itr->begin(); iitr != itr->end(); ++iitr){
+			if(iitr != itr->begin())std::cout << ",";
+			std::cout << *iitr;
+		}
+		std::cout << std::endl;
+	}
+}
 int main(){
 	std::ifstream fin("test1.csv");
 
-	auto Table = hmLib::read_csv("test1.csv",',','\n',true);
+	hmLib::table Table;
+	std::cout << std::endl << "=== true-false ===" << std::endl;
+	Table = hmLib::read_csv("test1.csv", true, false);
+	output_table(Table);
 
-	Table.push_back_column();
-	Table.insert_column(Table.column_begin() + 2);
+	std::cout << "===citr===" << std::endl;
+	output_table_citr(Table);
 
-	Table.push_back_row();
-	Table.insert_row(Table.row_begin() + 2);
-
-	Table.swap_column(Table.column_begin(), Table.column_begin() + 1);
-
-	Table.swap_row(Table.row_begin(), Table.row_begin() + 1);
-
-	std::cout <<Table.row_size()<<","<< Table.column_size() << std::endl;
-
-	for(unsigned int x = 0; x < Table.row_size(); ++x){
-		for(unsigned int y = 0; y < Table.column_size(); ++y){
-			std::cout << "[" << x << "," << y << "]: " << Table.at(x, y).ref() << std::endl;
-		}
-	}
+	std::cout << "===ritr===" << std::endl;
+	output_table_ritr(Table);
 
 	system("pause");
 	
-	return 0;
-/*
-	hmLib::icsv_iterator itr(fin, ',');
-	hmLib::ocsv_iterator oitr(std::cout, '\t');
-
-
-	hmLib::advance_line(itr);
-
-	while(!itr.eof()){
-
-		std::string Name;
-		unsigned int Val;
-		unsigned char Sex;
-		*itr++ >> Name;
-		*itr++ >> Val;
-		*itr++ >> Sex;
-
-		*oitr++ << Name;
-		*oitr++ << Val;
-		*oitr++ << Sex;
-		oitr.endl();
-	}
-
-	system("pause");
-
-
-
-	data_type Data;
-
-	column_proxy cp(Data);
-
-	auto bitr = cp.begin();
-	auto eitr = cp.end();
-*/
-
 	return 0;
 }
