9a4505290c12fccec6b5d838253866d921fb4fd5
hmito <hmito@outlook.com>
2016-06-18 00:24:30 +0100

    Add sequence.hpp, which control the discrete system like odeint.

---
+++ hmLib/sequence.hpp
@@ -0,0 +1,29 @@
+﻿#ifndef HMLIB_SEQUENCE_INC
+#define HMLIB_SEQUENCE_INC 100
+#
+namespace hmLib{
+	namespace sequence{
+		template<typename system,typename state>
+		void progress(system System, state& State, unsigned int BeginStep, unsigned int EndStep){
+			for(; BeginStep != EndStep; ++BeginStep){
+				System(State, BeginStep);
+			}
+		}
+		template<typename system, typename state, typename observer>
+		void progress(system System, state& State, unsigned int BeginStep, unsigned int EndStep, observer Observer){
+			for(; BeginStep != EndStep; ++BeginStep){
+				System(State, BeginStep);
+				Observer(State, BeginStep);
+			}
+		}
+		template<typename system, typename state, typename interruptable_observer>
+		void interruptable_progress(system System, state& State, unsigned int BeginStep, unsigned int EndStep, interruptable_observer IntrpObserver){
+			for(; BeginStep != EndStep; ++BeginStep){
+				System(State, BeginStep);
+				if(IntrpObserver(State, BeginStep))return;
+			}
+		}
+	}
+}
+#
+#endif
