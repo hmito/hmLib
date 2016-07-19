a5f219b565535edc0aa36e303b08f1547166be09
hmito <hmito@outlook.com>
2016-07-19 16:33:37 +0100

    Add include of new odeint files in odeint.hpp.

--- hmLib/odeint.hpp
+++ hmLib/odeint.hpp
@@ -2,5 +2,11 @@
 #define HMLIB_ODEINT_INC 100
 #
 #include"odeint/break_observer.hpp"
+#include"odeint/breakable_integrate.hpp"
+#include"odeint/container_observer.hpp"
+#include"odeint/eqstate_break_observer.hpp"
+#include"odeint/iterator_observer.hpp"
+#include"odeint/range_stepper.hpp"
+#include"odeint/stream_observer.hpp"
 #
 #endif
