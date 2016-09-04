5e65ae2b56e6440e284b01a6205ac2e5f3cac93a
hmito <hmito@outlook.com>
2016-09-04 22:59:29 +0100

    Change from mixin to inheritance

--- hmLib/enumerator.hpp
+++ hmLib/enumerator.hpp
@@ -222,145 +222,144 @@ namespace hmLib{
 				static_cast<enumerator_*>(this)->Concept.find_if(std::move(Pred), Target);
 			}
 		};
-	template<typename T, typename concept>
-	struct enumerator : public concept::mixin<enumerator<T, concept>>{
-		using mixin = concept::mixin;
-	private:
-		struct basic_enumerator_adaptor{};
-		template<typename iterator_>
-		struct enumerator_adaptor 
-			: public basic_enumerator_adaptor
-			, public concept{
-			using iterator = iterator_;
-		public:
-			iterator Begin;
-			iterator End;
-			iterator Current;
+		template<typename T, typename concept>
+		struct enumerator : public concept::mixin<enumerator<T, concept>>{
+			using mixin = concept::mixin;
+		private:
+			struct basic_enumerator_adaptor{};
+			template<typename iterator_>
+			struct enumerator_adaptor 
+				: public basic_enumerator_adaptor
+				, public concept{
+				using iterator = iterator_;
+			public:
+				iterator Begin;
+				iterator End;
+				iterator Current;
+			};
 		};
-	};
 
-	template<typename T, typename Concept = enumerators::default_concept<T>>
-	struct foward_enumeratort: public Concept{
-	private:
-		struct holder_interface : public Concept::mixin_interface{
+		template<typename T, typename Concept = enumerators::default_concept<T>>
+		struct foward_enumeratort: public Concept{
+		private:
+			struct holder_interface : public Concept::mixin_interface{
+			public:
+				virtual const T& operator()(void) const = 0;
+				virtual T& operator()(void) = 0;
+				virtual operator bool()const = 0;
+				virtual void operator++() = 0;
+				virtual void reset() = 0;
+			};
+		private:
+			holder_interface& ref;
 		public:
-			virtual const T& operator()(void) const = 0;
-			virtual T& operator()(void) = 0;
-			virtual operator bool()const = 0;
-			virtual void operator++() = 0;
+			template<typename iterator_>
+			struct holder :public  holder_interface, public Concept::mixin<holder<iterator_>> {
+				iterator_ begin;
+				iterator_ end;
+				iterator_ itr;
+			public:
+				holder(iterator_ begin_, iterator_ end_)
+					: begub(begin_)
+					, end(end_)
+					, itr(begin_){}
+			public:
+				const T& operator()(void) const override{ return *itr; }
+				T& operator()(void) override{ return *itr; }
+				operator bool()const override{ return itr != end; }
+				void operator++() override{ ++itr; }
+				void reset() override{ begin = itr; }
+			};
+		public:
+			foward_enumeratort(holder_interface& ref_) :ref(ref_){}
+		public:
+			const T& operator()(void) const{ return ref(); }
+			T& operator()(void){ return ref(); }
+			operator bool()const{ return static_cast<bool>(ref()); }
+			void operator++(){ ++ref; }
+			void reset(){ ref.reset(); }
+		};
+
+		//enumerator
+		template<typename T, typename concept = enumerators::default_concept<T> >
+		struct enumerator : public concept{
+			using this_type = enumerator<T, concept>;
+		private:
+			struct basic_enumeratable{
+			public:
+				virtual basic_enumeratable* clone()const = 0;
+				virtual void destroy() = 0;
+				virtual bool equal(const basic_enumeratable& itr)const = 0;
+				virtual void increment() = 0;
+				virtual const value_type& dereference()const = 0;
+			public:
+				template<typename enumerator_>
+				static basic_enumeratable* construct(enumerator_ itr);
+			};
+			template<typename iterator>
+			struct enumeratable :public basic_enumeratable{
+				typedef enumeratable<iterator> adaptor;
+			private:
+				iterator Begin;
+				iterator End;
+				iterator Current;
+			public:
+				basic_enumeratable* clone()const override{ return adaptor::construct(itr); }
+				void destroy() override{ adaptor::destruct(this); }
+				bool equal(const basic_enumeratable& itr_)const override{
+					const adaptor* adp = static_cast<const adaptor*>(&itr_);
+					if(adp == nullptr)return false;
+					return itr == adp->itr;
+				}
+				void increment() override{ ++itr; }
+				const value_type& dereference()const override{ return *itr; }
+			public:
+				enumerator_adaptor(enumerator_ itr_) :itr(itr_){}
+			private:
+				static boost::fast_pool_allocator<adaptor> allocator;
+			public:
+				static void destruct(adaptor* Ptr){
+					allocator.destroy(Ptr);
+					allocator.deallocate(Ptr);
+				}
+				static basic_enumeratable* construct(enumerator_ titr_){
+					adaptor* ans = allocator.allocate();
+					allocator.construct(ans, adaptor(titr_));
+					return ans;
+				}
+			};
+		private:
+			std::unique_ptr<basic_enumeratable, typename basic_enumeratable::deleter> Ptr;
+		public:
+			//return true if the object can be accessed to.
+			virtual operator bool()const{ return (*Ptr; }
+			//access to object.
+			virtual T& operator*(){ return Ptr->operator*(); }
+			//move to next object.
+			virtual void operator++(){ return Ptr->operator ++; }
+			//restart from the initial object.
 			virtual void reset() = 0;
-		};
-	private:
-		holder_interface& ref;
-	public:
-		template<typename iterator_>
-		struct holder :public  holder_interface, public Concept::mixin<holder<iterator_>> {
-			iterator_ begin;
-			iterator_ end;
-			iterator_ itr;
-		public:
-			holder(iterator_ begin_, iterator_ end_)
-				: begub(begin_)
-				, end(end_)
-				, itr(begin_){}
-		public:
-			const T& operator()(void) const override{ return *itr; }
-			T& operator()(void) override{ return *itr; }
-			operator bool()const override{ return itr != end; }
-			void operator++() override{ ++itr; }
-			void reset() override{ begin = itr; }
-		};
-	public:
-		foward_enumeratort(holder_interface& ref_) :ref(ref_){}
-	public:
-		const T& operator()(void) const{ return ref(); }
-		T& operator()(void){ return ref(); }
-		operator bool()const{ return static_cast<bool>(ref()); }
-		void operator++(){ ++ref; }
-		void reset(){ ref.reset(); }
-	};
-
-	//enumerator
-	template<typename T, typename concept = enumerators::default_concept<T> >
-	struct enumerator : public concept{
-		using this_type = enumerator<T, concept>;
-	private:
-		struct basic_enumeratable{
-		public:
-			virtual basic_enumeratable* clone()const = 0;
-			virtual void destroy() = 0;
-			virtual bool equal(const basic_enumeratable& itr)const = 0;
-			virtual void increment() = 0;
-			virtual const value_type& dereference()const = 0;
-		public:
+		public://for conctructor
+			enumerator() :Ptr(nullptr){}
+			enumerator(const my_type& My_) :Ptr(nullptr){ if(My_.Ptr)Ptr.reset(My_.Ptr->clone()); }
+			const my_type& operator=(const my_type& My_){
+				if(this != &My_)Ptr.reset(My_.Ptr->clone());
+				return *this;
+			}
+			enumerator(my_type&& My_) :Ptr(std::move(My_.Ptr)){}
+			const my_type& operator=(my_type&& My_){
+				if(this != &My_)Ptr = std::move(My_.Ptr);
+				return *this;
+			}
 			template<typename enumerator_>
-			static basic_enumeratable* construct(enumerator_ itr);
-		};
-		template<typename iterator>
-		struct enumeratable :public basic_enumeratable{
-			typedef enumeratable<iterator> adaptor;
-		private:
-			iterator Begin;
-			iterator End;
-			iterator Current;
-		public:
-			basic_enumeratable* clone()const override{ return adaptor::construct(itr); }
-			void destroy() override{ adaptor::destruct(this); }
-			bool equal(const basic_enumeratable& itr_)const override{
-				const adaptor* adp = static_cast<const adaptor*>(&itr_);
-				if(adp == nullptr)return false;
-				return itr == adp->itr;
-			}
-			void increment() override{ ++itr; }
-			const value_type& dereference()const override{ return *itr; }
-		public:
-			enumerator_adaptor(enumerator_ itr_) :itr(itr_){}
-		private:
-			static boost::fast_pool_allocator<adaptor> allocator;
-		public:
-			static void destruct(adaptor* Ptr){
-				allocator.destroy(Ptr);
-				allocator.deallocate(Ptr);
-			}
-			static basic_enumeratable* construct(enumerator_ titr_){
-				adaptor* ans = allocator.allocate();
-				allocator.construct(ans, adaptor(titr_));
-				return ans;
+			enumerator(enumerator_ itr_) : Ptr(basic_enumerator_adaptor::construct(itr_)){}
+			template<typename enumerator_>
+			const my_type& operator=(enumerator_ itr_){
+				Ptr.reset(basic_enumerator_adaptor::construct(itr));
+				return *this;
 			}
 		};
-	private:
-		std::unique_ptr<basic_enumeratable, typename basic_enumeratable::deleter> Ptr;
-	public:
-		//return true if the object can be accessed to.
-		virtual operator bool()const{ return (*Ptr; }
-		//access to object.
-		virtual T& operator*(){ return Ptr->operator*(); }
-		//move to next object.
-		virtual void operator++(){ return Ptr->operator ++; }
-		//restart from the initial object.
-		virtual void reset() = 0;
-	public://for conctructor
-		enumerator() :Ptr(nullptr){}
-		enumerator(const my_type& My_) :Ptr(nullptr){ if(My_.Ptr)Ptr.reset(My_.Ptr->clone()); }
-		const my_type& operator=(const my_type& My_){
-			if(this != &My_)Ptr.reset(My_.Ptr->clone());
-			return *this;
-		}
-		enumerator(my_type&& My_) :Ptr(std::move(My_.Ptr)){}
-		const my_type& operator=(my_type&& My_){
-			if(this != &My_)Ptr = std::move(My_.Ptr);
-			return *this;
-		}
-		template<typename enumerator_>
-		enumerator(enumerator_ itr_) : Ptr(basic_enumerator_adaptor::construct(itr_)){}
-		template<typename enumerator_>
-		const my_type& operator=(enumerator_ itr_){
-			Ptr.reset(basic_enumerator_adaptor::construct(itr));
-			return *this;
-		}
-	};
 	}
-
 }
 #
 #endif
--- vc/hmLib/hmLib/lab_enumerator.cpp
+++ vc/hmLib/hmLib/lab_enumerator.cpp
@@ -6,6 +6,7 @@
 #include<iterator>
 #include<type_traits>
 #include<hmLib/type_traits.hpp>
+
 /*
 namespace enumerators{
 	template<typename type, typename base, typename... others>
@@ -70,18 +71,11 @@ namespace enumerators{
 	};
 }
 
-template<typename T, typename... traits>
-struct basic_sentinel_enumerator :public enumerators::traits_mixin<T, basic_sentinel_enumerator<T, traits...>, traits...>{
-	using interface = enumerators::traits_interface<T, traits...>;
+template<typename T, typename enumeratable_, typename... traits>
+struct basic_enumerator :public enumerators::traits_mixin<T, basic_enumerator<T, enumeratable_, traits...>, traits...>{
+	using interface = enumerators::traits_interface<T, enumeratable_, traits...>;
 	template<typename iterator_>
-	struct enumeratable :public enumerators::traits_impl<T, enumeratable<iterator_>, interface, traits...>{
-	public:
-		iterator_ Cur;
-		iterator_ Beg;
-		iterator_ End;
-	public:
-		enumeratable(iterator_ Beg_, iterator_ End_) :Cur(Beg_), Beg(Beg_), End(End_){}
-	};
+	using enumeratable = enumerators::traits_impl<T, enumeratable_::impl<iterator_>, interface, traits...>;
 private:
 	std::unique_ptr<interface> Ptr;
 public:
@@ -94,45 +88,308 @@ public:
 	}
 };
 
-*/
+//enumerator tag
+struct referable_enumeratable_tag{};
+struct sentinel_enumeratable_tag :public referable_enumeratable_tag{};
+struct range_enumeratable_tag :public sentinel_enumeratable_tag{};
+struct mutable_range_enumeratable_tag :public range_enumeratable_tag{};
 
-struct referable_enumeratable{
-	static void out(){ std::cout << "referable" << std::endl; }
-	template<typename iterator_, typename interface_>
-	struct base:public interface_{
-		iterator_ Cur;
-	};
-};
-struct sentinel_enumeratable:public referable_enumeratable{
-	static void out(){ std::cout << "sentinel" << std::endl; }
-};
-struct range_enumeratable:public sentinel_enumeratable{
-	static void out(){ std::cout << "range" << std::endl; }
-};
-struct mutable_range_enumeratable:public range_enumeratable{
-	static void out(){ std::cout << "mutable_range" << std::endl; }
-};
 
 
 struct find_trait{
-	template<typename enumeratable_, typename enumeratable_type = hmLib::sieve_base_of<enumeratable_, sentinel_enumeratable, mutable_range_enumeratable>::type>
+	template<
+		typename enumeratable_, 
+		typename enumeratable_type = hmLib::sieve_base_of<enumeratable_, sentinel_enumeratable_tag, mutable_range_enumeratable_tag>::type
+	>
 	struct impl{};
 	template<typename enumeratable_>
-	struct impl<enumeratable_, sentinel_enumeratable>{
+	struct impl<enumeratable_, sentinel_enumeratable_tag>{
 		static void str(){ std::cout << "snetinel" << std::endl; }
 	};
 	template<typename enumeratable_>
-	struct impl<enumeratable_, mutable_range_enumeratable>{
+	struct impl<enumeratable_, mutable_range_enumeratable_tag>{
 		static void str(){ std::cout << "mutable_range" << std::endl; }
 	};
 };
+
+struct sort_trait{
+	template<typename type>
+	struct enumeratable_interface{
+	public:
+		virtual void sort(void) = 0;
+	};
+	template<typename type, typename enumeratable, typename base>
+	struct enumeratable_mixin : public base{
+	public:
+		void sort(void)override{ auto& ref = static_cast<enumeratable&>(*this); std::sort(ref.Begin, ref.End); }
+	};
+	template<typename type, typename base>
+	struct enumerator_mixin{
+	public:
+		void sort(void){ auto& ref = static_cast<base*>(this)->get_enumeratable(); ref.sort(); }
+	};
+};
+
+struct incrementable_traversal_traits{
+	template<typename type>
+	struct enumeratable_interface{
+	public:
+		virtual type& operator*(void) = 0;
+		virtual const type& operator*(void) const = 0;
+		virtual void operator++() = 0;
+	};
+	template<typename type, typename enumeratable, typename base>
+	struct enumeratable_mixin : public base{
+	private:
+		enumeratable& Ref;
+	public:
+		enumeratable_mixin():Ref(static_cast<enumeratable&>(*this)){}
+	public:
+		type& operator*(void) override{ return *(Ref.Cur); }
+		const type& operator*(void) const override{ return *(Ref.Cur); }
+		void operator++() override{ ++(Ref.Cur); }
+	};
+	template<typename type, typename base>
+	struct enumerator_mixin{
+		decltype(static_cast<base*>(this)->get_enumeratable()) Ref;
+	private:
+		auto& ref()->{ return  static_cast<base*>(this)->get_enumeratable(); }
+		const auto& ref()const->decltype(static_cast<base*>(this)->get_enumeratable()){ return  static_cast<base*>(this)->get_enumeratable(); }
+	public:
+		enumerator_mixin() :Ref(static_cast<enumeratable&>(*this)){}
+	public:
+		type& operator*(void){ return *ref(); }
+		const type& operator*(void) const{ return *ref(); }
+		operator bool()const{ return static_cast<bool>(ref()); }
+		void operator++(){ ++ref(); }
+		void reset(){ ref().reset(); }
+	};
+};
+struct forward_traversal_traits{
+	template<typename type>
+	struct enumeratable_interface{
+	public:
+		virtual type& operator*(void) = 0;
+		virtual const type& operator*(void) const = 0;
+		virtual operator bool()const = 0;
+		virtual void operator++() = 0;
+		virtual void reset() = 0;
+	};
+	template<typename type, typename enumeratable, typename base>
+	struct enumeratable_mixin : public base{
+	private:
+		enumeratable& ref(){return static_cast<enumeratable&>(*this);}
+		const enumeratable& ref()const{ return static_cast<const enumeratable&>(*this); }
+	public:
+		type& operator*(void) override{ return *(ref().Cur); }
+		const type& operator*(void) const override{ return *(ref().Cur); }
+		operator bool()const override{ const auto& Ref = ref(); return Ref.Cur != Ref.End; }
+		void operator++() override{ ++(ref().Cur); }
+		void reset() override{ auto& Ref = ref(); ref.Cur = ref.Beg; }
+	};
+	template<typename type, typename base>
+	struct enumerator_mixin{
+	private:
+		auto& ref()->decltype(static_cast<base*>(this)->get_enumeratable()){ return  static_cast<base*>(this)->get_enumeratable(); }
+		const auto& ref()const->decltype(static_cast<base*>(this)->get_enumeratable()){ return  static_cast<base*>(this)->get_enumeratable(); }
+	public:
+		type& operator*(void){ return *ref(); }
+		const type& operator*(void) const{return *ref(); }
+		operator bool()const{return static_cast<bool>(ref()); }
+		void operator++(){ ++ref(); }
+		void reset(){ ref().reset(); }
+	};
+};
+//*/
+//enumeratable
+namespace enumerators{
+	//enumerator tag
+	struct referable_enumerator_tag{};
+	struct sentinel_enumerator_tag :public referable_enumerator_tag{};
+	struct range_enumerator_tag :public sentinel_enumerator_tag{};
+	struct mutable_range_enumerator_tag :public range_enumerator_tag{};
+
+	//enumeratable
+	struct referable_enumeratable{
+		template<typename extention_interface_>
+		struct enumeratable_interface{};
+		template<typename iterator_, typename extention_interface_>
+		struct enumeratable : public enumeratable_interface, public extention_interface_{
+			iterator_ Cur;
+		};
+	};
+	struct sentinel_enumeratable{
+		template<typename extention_interface_>
+		struct enumeratable_interface : public referable_enumeratable::enumeratable_interface< extention_interface_>{
+			virtual operator bool()const = 0;
+		};
+		template<typename iterator_, typename extention_interface_>
+		struct enumeratable :public enumeratable_interface, public extention_interface_{
+			iterator_ Cur;
+			iterator_ End;
+		public:
+			operator bool()const override{
+				return Cur != End;
+			}
+		};
+	};
+	struct range_enumeratable{
+		template<typename extention_interface_>
+		struct enumeratable_interface : public sentinel_enumeratable::enumeratable_interface< extention_interface_>{
+			virtual void reset() = 0;
+		};
+		template<typename iterator_, typename extention_interface_>
+		struct enumeratable :public enumeratable_interface, public extention_interface_{
+			iterator_ Cur;
+			iterator_ Beg;
+			iterator_ End;
+		public:
+			operator bool()const override{
+				return Cur != End;
+			}
+			void reset()override{
+				Cur = Beg;
+			}
+		};
+	};
+	struct mutable_range_enumeratable{
+		template<typename extention_interface_>
+		struct enumeratable_interface : public range_enumeratable::enumeratable_interface< extention_interface_>{
+			virtual void reset_range() = 0;
+		};
+		template<typename iterator_, typename extention_interface_>
+		struct enumeratable :public enumeratable_interface, public extention_interface_{
+			iterator_ Cur;
+			iterator_ Beg;
+			iterator_ End;
+			iterator_ IniBeg;
+			iterator_ IniEnd;
+		public:
+			operator bool()const override{
+				return Cur != End;
+			}
+			void reset()override{
+				Cur = Beg;
+			}
+			void reset_range()override{
+				Beg = IniBeg;
+				End = IniEnd;
+			}
+		};
+	};
+
+	//enumeratable selecter
+	template<typename enumerator_tag>
+	struct select_enumeratable{};
+	template<>
+	struct select_enumeratable<referable_enumerator_tag>{
+		using type = referable_enumeratable;
+	};
+	template<>
+	struct select_enumeratable<sentinel_enumerator_tag>{
+		using type = sentinel_enumeratable;
+	};
+	template<>
+	struct select_enumeratable<range_enumerator_tag>{
+		using type = range_enumeratable;
+	};
+	template<>
+	struct select_enumeratable<mutable_range_enumerator_tag>{
+		using type = mutable_range_enumeratable;
+	};
+}
+//trait pack
+namespace enumerators{
+	template<typename... traits>
+	struct traits_pack{
+	private:
+		template<typename enumerator, typename... others>
+		struct extention_interface_pack{};
+		template<typename enumerator, typename trait, typename... others>
+		struct extention_interface_pack<enumerator, trait, others...>
+			: public trait::template extention_interface<enumerator>
+			, public extention_interface_pack<enumerator, others...>{
+		};
+		template<typename enumeratable, typename base, typename... others>
+		struct extention_pack{};
+		template<typename enumeratable, typename base, typename trait, typename... others>
+		struct extention_pack<enumeratable, base, trait, others...>
+			: public extention_pack<enumeratable, trait::template extention<enumeratable, base>, others...>{};
+		template<typename enumerator, typename base, typename... others>
+		struct enumerator_extention_pack{};
+		template<typename enumerator, typename base, typename trait, typename... others>
+		struct enumerator_extention_pack<enumerator, base, trait, others...>
+			: public enumerator_extention_pack<enumerator, trait::template enumerator_extention<enumerator, base>, others...>{};
+	public:
+		template<typename enumerator>
+		using extention_interface = extention_interface_pack<enumerator, traits...>;
+		template<typename enumeratable, typename base>
+		using extention = extention_pack<enumeratable, base, traits...>;
+		template<typename enumerator, typename base>
+		using enumerator_extention = enumerator_extention_pack<enumerator, base, traits...>;
+	};
+}
+struct trait1{
+	template<typename enumerator>
+	struct extention_interface{
+		virtual enumerator::value_type func1() = 0;
+	};
+	template<typename enumeratable, typename base>
+	struct extention : public base{
+		enumeratable::value_type func1()override{
+			return *enrable::Cur;
+		}
+	};
+	template<typename enumerator, typename base>
+	struct enumerator_extention : public base{
+		enumerator::value_type  func1(){
+			enr_t::ref().func1();
+		}
+	};
+};
+struct trait2{
+	template<typename enumerator>
+	struct extention_interface{
+		virtual enumerator::value_type func2() = 0;
+	};
+	template<typename enumeratable, typename base>
+	struct extention : public base{
+		enumeratable::value_type func2()override{
+			return *enumeratable::Beg;
+		}
+	};
+	template<typename enumerator, typename base>
+	struct enumerator_extention : public base{
+		enumerator::value_type func2(){
+			return enumerator::ref().func2();
+		}
+	};
+};
+
+
+template<typename enumerator_tag_, typename traversal_tag_, typename value_type_, typename difference_type_, typename reference_, typename pointer_, typename... traits>
+struct basic_enumerator{
+public:
+	using enumerator_tag = enumerator_tag_;
+	using traversal_tag = traversal_tag_;
+	using value_type = value_type_;
+	using difference_type = difference_type_;
+	using reference = reference_;
+	using pointer = pointer_;
+private:
+	using this_trais_pack = enumerators::traits_pack<traits...>;
+	using this_enumeratable = typename enumerators::select_enumeratable<enumerator_tag>::type;
+public:
+	using enumeratable_interface = typename this_enumeratable::template enumeratable_interface<typename this_trais_pack::extention_interface>;
+	template<typename iterator_>
+	using enumeratable = typename this_trais_pack::extention<enumeratable_, enumeratable_>;
+private:
+	std::unique_ptr<enumeratable_interface> Ptr;
+public:
+	template<typename iterator_>
+	static enumeratable<iterator_> make_enumeratable(iterator_ Begin, iterator_ End){
+	}
+};
 int main(void){
-//	find_trait::impl<referable_enumeratable>::str();
-	find_trait::impl<sentinel_enumeratable>::str();
-	find_trait::impl<range_enumeratable>::str();
-	find_trait::impl<mutable_range_enumeratable>::str();
-
-
-	system("pause");
 	return 0;
 }
