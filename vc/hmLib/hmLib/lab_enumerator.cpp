5142d5d8b6dadd80550f1de8a6436bb049f8547b
hmito <hmito@outlook.com>
2016-09-04 01:50:01 +0100

    Update for v4_03_03.

--- vc/hmLib/hmLib/lab_enumerator.cpp
+++ vc/hmLib/hmLib/lab_enumerator.cpp
@@ -5,6 +5,7 @@
 #include<iostream>
 #include<iterator>
 #include<type_traits>
+#include<hmLib/type_traits.hpp>
 /*
 namespace enumerators{
 	template<typename type, typename base, typename... others>
@@ -95,51 +96,12 @@ public:
 
 */
 
-
-
-template<typename type1, typename type2>
-struct select_derived{
-	template<typename type, bool Type1IsBase = std::is_base_of<type1,type2>::value, bool Type2IsBase = std::is_base_of<type2, type1>::value>
-	struct check{
-		using ans_type = void;
-	};
-	template<typename type>
-	struct check<type, true, false>{
-		using ans_type = type2;
-	};
-	template<typename type>
-	struct check<type, false, true>{
-		using ans_type = type1;
-	};
-	using type = typename check<void>::ans_type;
-};
-template<typename terget, typename... others>
-struct sieve_base_of{
-	template<typename terget_, typename candidate_>
-	struct check{
-		using ans_type = candidate_;
-	};
-	using type = typename check<terget, void>::ans_type;
-};
-template<typename terget, typename try_type, typename... others>
-struct sieve_base_of<terget, try_type, others...>{
-	template<typename terget_, typename candidate_, bool IsBase = std::is_base_of<try_type, terget>::value>
-	struct check{
-		using ans_type = typename sieve_base_of<terget, others...>::template check<terget_, candidate_>::ans_type;
-	};
-	template<typename terget_, typename candidate_>
-	struct check<terget_, candidate_, true>{
-		using new_candidate = typename select_derived<candidate_, try_type>::type;
-		using ans_type = typename sieve_base_of<terget, others...>::template check<terget_, new_candidate>::ans_type;
-	};
-	template<typename terget_>
-	struct check<terget_, void, true>{
-		using ans_type = typename sieve_base_of<terget, others...>::template check<terget_, try_type>::ans_type;
-	};
-	using type = typename check<terget, void>::ans_type;
-};
 struct referable_enumeratable{
 	static void out(){ std::cout << "referable" << std::endl; }
+	template<typename iterator_, typename interface_>
+	struct base:public interface_{
+		iterator_ Cur;
+	};
 };
 struct sentinel_enumeratable:public referable_enumeratable{
 	static void out(){ std::cout << "sentinel" << std::endl; }
@@ -153,7 +115,7 @@ struct mutable_range_enumeratable:public range_enumeratable{
 
 
 struct find_trait{
-	template<typename enumeratable_, typename enumeratable_type = sieve_base_of<enumeratable_, mutable_range_enumeratable, sentinel_enumeratable>::type>
+	template<typename enumeratable_, typename enumeratable_type = hmLib::sieve_base_of<enumeratable_, sentinel_enumeratable, mutable_range_enumeratable>::type>
 	struct impl{};
 	template<typename enumeratable_>
 	struct impl<enumeratable_, sentinel_enumeratable>{
