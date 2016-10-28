927225c9b7dce1bbc141f821e1658e730fb38c1b
hmito <hmito@outlook.com>
2016-10-28 19:19:00 +0100

    Add chain::iterator

--- hmLib/chain.hpp
+++ hmLib/chain.hpp
@@ -35,18 +35,105 @@ namespace hmLib{
 			}
 		};
 	public:
-		struct iterator{};
-		struct const_iterator{};
+		struct iterator:public std::iterator<std::bidirectional_iterator_tag,T>{
+			friend struct const_iterator;
+		private:
+			element* Cur;
+		public:
+			iterator():Cur(0){}
+			explicit iterator(element* Cur_):Cur(Cur_){}
+			iterator(const iterator& Other):Cur(Other.Cur){}
+			iterator& operator=(const iterator& Other){
+				if(this != &Other){
+					Cur = Other.Cur;
+				}
+				return *this;
+			}
+		public:
+			iterator& operator++(){
+				Cur = Cur->next;
+				return *this;
+			}
+			iterator operator++(int){
+				iterator Ans(*this);
+				this->operator++();
+				return Ans;
+			}
+			iterator& operator--(){
+				Cur = Cur->prev;
+				return *this;
+			}
+			iterator operator--(int){
+				iterator Ans(*this);
+				this->operator--();
+				return Ans;
+			}
+			T& operator*(){ return Cur->operator*(); }
+			const T& operator*()const{ return Cur->operator*(); }
+			T* operator->(){ return Cur->operator->(); }
+			const T* operator->()const{ return Cur->operator->(); }
+			friend bool operator==(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur == Itr2.Cur;
+			}
+			friend bool operator!=(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur != Itr2.Cur;
+			}
+		};
+		struct const_iterator :public std::iterator<std::bidirectional_iterator_tag, const T>{
+		private:
+			element* Cur;
+		public:
+			const_iterator() :Cur(0){}
+			explicit const_iterator(element* Cur_) :Cur(Cur_){}
+			const_iterator(const iterator& Other) :Cur(Other.Cur){}
+			const_iterator(const const_iterator& Other) :Cur(Other.Cur){}
+			const_iterator& operator=(const iterator& Other){
+				if(this != &Other){
+					Cur = Other.Cur;
+				}
+				return *this;
+			}
+		public:
+			const_iterator& operator++(){
+				Cur = Cur->next;
+				return *this;
+			}
+			const_iterator operator++(int){
+				iterator Ans(*this);
+				this->operator++();
+				return Ans;
+			}
+			const_iterator& operator--(){
+				Cur = Cur->prev;
+				return *this;
+			}
+			const_iterator operator--(int){
+				iterator Ans(*this);
+				this->operator--();
+				return Ans;
+			}
+			const T& operator*()const{ return Cur->operator*(); }
+			const T* operator->()const{ return Cur->operator->(); }
+			friend bool operator==(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur == Itr2.Cur;
+			}
+			friend bool operator!=(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur != Itr2.Cur;
+			}
+		};
 	private:
 		element Sentinel;
 		size_type Size;
 	public:
-		chain():Size(0){}
+		chain():Size(0){
+			Sentinel.next = &Sentinel;
+			Sentinel.prev= &Sentinel;
+		}
 	public:
-		iterator begin();
-		iterator end();
-		const_iterator cbegin()const;
-		const_iterator cend()const;
+		iterator begin(){ return iterator(Sentinel.next); }
+		iterator end(){ return iterator(&Sentinel); }
+		const_iterator cbegin()const{ return const_iterator(Sentinel.next); }
+		const_iterator cend()const{ return const_iterator(&Sentinel); }
 		const_iterator begin()const{ return cbegin(); }
 		const_iterator end()const{ return cend(); }
 	public:
