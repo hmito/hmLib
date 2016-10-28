0abaae5734bb66054b1cb8ad3602220feb2f6688
hmito <hmito@outlook.com>
2016-10-28 16:59:35 +0100

    Add size and empty in chain.

--- hmLib/chain.hpp
+++ hmLib/chain.hpp
@@ -1,85 +1,111 @@
-template<typename T>
-struct chain{
-public:
-	using this_type = chain<T>;
-	using size_type = unsigned int;
-public:
-	struct element{
-	private:
-		T value;
-		element *next;
+#include<functional>
+namespace hmLib{
+	template<typename T>
+	struct chain{
 	public:
-		operator bool(){ return next != 0; }
-		T& operator*(){ return value; }
-		const T& operator*()const{ return value; }
-		T* operator->(){ return &value; }
-		const T* operator->()const{ return &value; }
-	};
-	struct iterator{};
-	struct const_iterator{};
-private:
-	element Sentinel;
-	size_type Size;
-public:
-	iterator begin();
-	iterator end();
-	const_iterator cbegin()const;
-	const_iterator cend()const;
-	const_iterator begin()const{ return cbegin(); }
-	const_iterator end()const{ return cend(); }
-public:
-	bool empty();
-	size_type size();
-	T& front();
-	T& back();
-	const T& front()const;
-	const T& back()const;
-	void push_back(element& Elem);
-	void pop_back();
-	void push_front(element& Elem);
-	void pop_front();
-	iterator insert(const_iterator pos, element& Elem);
-	iterator erase(const_iterator pos);
-	iterator erase(const_iterator pos, const_iterator last);
-	void swap(this_type& other);
-	void clear();
-	void splice(const_iterator pos, this_type& other);
-};
-
-template<typename T, typename Comp = >
-struct priority_chain{
-public:
-	using this_type = chain<T>;
-	using size_type = unsigned int;
-public:
-	struct element{
-	private:
-		T value;
-		element *next;
+		using this_type = chain<T>;
+		using size_type = unsigned int;
 	public:
-		operator bool(){ return next != 0; }
-		T& operator*(){ return value; }
-		const T& operator*()const{ return value; }
-		T* operator->(){ return &value; }
-		const T* operator->()const{ return &value; }
+		struct element{
+			friend struct chain<T>;
+		private:
+			T value;
+			element *prev;
+			element *next;
+		public:
+			element() :prev(0), next(0){}
+			explicit element(const T& value_) :value(value_), prev(0), next(0){}
+			element(const element& Elm) :value(Elm.value), prev(0), next(0){}
+			element& operator=(const element& Elm){
+				if(this != &Elm){
+					value = Elm.value;
+				}
+				return *this;
+			}
+		public:
+			operator bool(){ return next == 0; }
+			T& operator*(){ return value; }
+			const T& operator*()const{ return value; }
+			T* operator->(){ return &value; }
+			const T* operator->()const{ return &value; }
+		public:
+			static void connect(element& Prev, element& Next){
+				Prev.next = &Next;
+				Next.prev = &Prev;
+			}
+		};
+	public:
+		struct iterator{};
+		struct const_iterator{};
+	private:
+		element Sentinel;
+		size_type Size;
+	public:
+		chain():Size(0){}
+	public:
+		iterator begin();
+		iterator end();
+		const_iterator cbegin()const;
+		const_iterator cend()const;
+		const_iterator begin()const{ return cbegin(); }
+		const_iterator end()const{ return cend(); }
+	public:
+		bool empty(){ return Size==0; }
+		size_type size(){ return Size; }
+		T& front();
+		T& back();
+		const T& front()const;
+		const T& back()const;
+		void push_back(element& Elem){
+			element& Prev = *(Sentinel.prev);
+			element::connect(Prev, Elem);
+			element::connect(Elem, Sentinel);
+		}
+		void pop_back();
+		void push_front(element& Elem);
+		void pop_front();
+		iterator insert(const_iterator pos, element& Elem);
+		iterator erase(const_iterator pos);
+		iterator erase(const_iterator pos, const_iterator last);
+		void swap(this_type& other);
+		void clear();
+		void splice(const_iterator pos, this_type& other);
 	};
-	struct iterator{};
-	struct const_iterator{};
-private:
-	element Sentinel;
-public:
-	iterator begin();
-	iterator end();
-	const_iterator cbegin()const;
-	const_iterator cend()const;
-	const_iterator begin()const{ return cbegin(); }
-	const_iterator end()const{ return cend(); }
-public:
-	bool empty();
-	T& front();
-	const T& front()const;
-	void push(element& Elem);
-	void pop();
-	void swap(this_type& other);
-	void clear();
-};
+	template<typename T, typename Comp = std::less<T>>
+	struct priority_chain{
+	public:
+		using this_type = chain<T>;
+		using size_type = unsigned int;
+	public:
+		struct element{
+		private:
+			T value;
+			element *next;
+		public:
+			operator bool(){ return next != 0; }
+			T& operator*(){ return value; }
+			const T& operator*()const{ return value; }
+			T* operator->(){ return &value; }
+			const T* operator->()const{ return &value; }
+		};
+		struct iterator{};
+		struct const_iterator{};
+	private:
+		element Sentinel;
+	public:
+		iterator begin();
+		iterator end();
+		const_iterator cbegin()const;
+		const_iterator cend()const;
+		const_iterator begin()const{ return cbegin(); }
+		const_iterator end()const{ return cend(); }
+	public:
+		bool empty();
+		T& front();
+		const T& front()const;
+		void push(element& Elem);
+		void pop();
+		void swap(this_type& other);
+		void clear();
+	};
+}
