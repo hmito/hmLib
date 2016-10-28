7a7156ca7a8604a3e77b896d1acc26c18d822422
hmito <hmito@outlook.com>
2016-10-28 21:01:01 +0100

    Add priority_chain functions except for push and clear.

--- hmLib/chain.hpp
+++ hmLib/chain.hpp
@@ -242,41 +242,135 @@ namespace hmLib{
 			Size += AddSize;
 		}
 	};
-	template<typename T, typename Comp = std::less<T>>
+	template<typename T, typename compare_ = std::less<T>>
 	struct priority_chain{
 	public:
 		using this_type = chain<T>;
 		using size_type = unsigned int;
+		using compare = compare_;
 	public:
 		struct element{
+			friend struct chain<T>;
 		private:
 			T value;
 			element *next;
 		public:
-			operator bool(){ return next != 0; }
+			element() :next(0){}
+			explicit element(const T& value_) :value(value_), next(0){}
+			element(const element& Elm) :value(Elm.value), next(0){}
+			element& operator=(const element& Elm){
+				if(this != &Elm){
+					value = Elm.value;
+				}
+				return *this;
+			}
+		public:
+			operator bool(){ return next == 0; }
 			T& operator*(){ return value; }
 			const T& operator*()const{ return value; }
 			T* operator->(){ return &value; }
 			const T* operator->()const{ return &value; }
+		public:
+			static void connect(element& Prev, element& Next){
+				Prev.next = &Next;
+			}
 		};
-		struct iterator{};
-		struct const_iterator{};
+	public:
+		struct iterator :public std::iterator<std::forward_iterator_tag, T>{
+		private:
+			element* Cur;
+		public:
+			iterator() :Cur(0){}
+			explicit iterator(element* Cur_) :Cur(Cur_){}
+			iterator(const iterator& Other) :Cur(Other.Cur){}
+			iterator& operator=(const iterator& Other){
+				if(this != &Other){
+					Cur = Other.Cur;
+				}
+				return *this;
+			}
+		public:
+			iterator& operator++(){
+				Cur = Cur->next;
+				return *this;
+			}
+			iterator operator++(int){
+				iterator Ans(*this);
+				this->operator++();
+				return Ans;
+			}
+			T& operator*(){ return Cur->operator*(); }
+			const T& operator*()const{ return Cur->operator*(); }
+			T* operator->(){ return Cur->operator->(); }
+			const T* operator->()const{ return Cur->operator->(); }
+			friend bool operator==(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur == Itr2.Cur;
+			}
+			friend bool operator!=(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur != Itr2.Cur;
+			}
+		public:
+			element* current(){ return Cur; }
+		};
+		struct const_iterator :public std::iterator<std::forward_iterator_tag, const T>{
+		private:
+			element* Cur;
+		public:
+			const_iterator() :Cur(0){}
+			explicit const_iterator(element* Cur_) :Cur(Cur_){}
+			const_iterator(const iterator& Other) :Cur(Other.current()){}
+			const_iterator(const const_iterator& Other) :Cur(Other.Cur){}
+			const_iterator& operator=(const iterator& Other){
+				if(this != &Other){
+					Cur = Other.Cur;
+				}
+				return *this;
+			}
+		public:
+			const_iterator& operator++(){
+				Cur = Cur->next;
+				return *this;
+			}
+			const_iterator operator++(int){
+				iterator Ans(*this);
+				this->operator++();
+				return Ans;
+			}
+			const T& operator*()const{ return Cur->operator*(); }
+			const T* operator->()const{ return Cur->operator->(); }
+			friend bool operator==(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur == Itr2.Cur;
+			}
+			friend bool operator!=(const iterator& Itr1, const iterator& Itr2){
+				return Itr1.Cur != Itr2.Cur;
+			}
+		public:
+			element* current(){ return Cur; }
+		};
 	private:
 		element Sentinel;
+		compare Comp;
 	public:
-		iterator begin();
-		iterator end();
-		const_iterator cbegin()const;
-		const_iterator cend()const;
+		iterator begin(){ return iterator(Sentinel.next); }
+		iterator end(){ return iterator(&Sentinel); }
+		const_iterator cbegin()const{ return const_iterator(Sentinel.next); }
+		const_iterator cend()const{ return const_iterator(&Sentinel); }
 		const_iterator begin()const{ return cbegin(); }
 		const_iterator end()const{ return cend(); }
 	public:
-		bool empty();
-		T& front();
-		const T& front()const;
+		bool empty(){ return Sentinel.next == &Sentinel; }
+		T& front(){ return *(*(Sentinel.next)); }
+		const T& front()const{ return *(*(Sentinel.next)); }
 		void push(element& Elem);
-		void pop();
-		void swap(this_type& other);
+		void pop(){
+			element& Next = *(Sentinel.next);
+			element::connect(Sentinel, *(Next.next));
+			Next.next = 0;
+		}
+		void swap(this_type& other){
+			std::swap(Sentinel.next, other.Sentinel.next);
+			std::swap(Size, other.Size);
+		}
 		void clear();
 	};
 }
