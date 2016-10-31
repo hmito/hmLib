a00f3a2e715b87de0689575fc02ad43d101efa61
hmito <hmito@outlook.com>
2016-10-31 13:41:00 +0000

    Make directory for sequence.hpp

---
+++ hmLib/sequence/progess.hpp
@@ -0,0 +1,22 @@
+﻿#ifndef HMLIB_SEQUENCE_INC
+#define HMLIB_SEQUENCE_INC 100
+#
+namespace hmLib{
+	namespace sequence{
+		template<typename system,typename state>
+		void progress(system System, state& State, unsigned int BeginStep, unsigned int EndStep){
+			for(; BeginStep != EndStep; ++BeginStep){
+				System(State, BeginStep);
+			}
+		}
+		template<typename system, typename state, typename observer>
+		void progress(system System, state& State, unsigned int BeginStep, unsigned int EndStep, observer Observer){
+			for(; BeginStep != EndStep; ++BeginStep){
+				System(State, BeginStep);
+				Observer(State, BeginStep);
+			}
+		}
+	}
+}
+#
+#endif
