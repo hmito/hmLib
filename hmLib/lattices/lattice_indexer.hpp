9fa49cbc379a50d466a6ec39e5cb4de9e6e31afa
hmito <hmito@outlook.com>
2017-03-22 19:26:24 +0000

    lattice_indexer is now based on the gap_lattice_indexer.

--- hmLib/lattices/lattice_indexer.hpp
+++ hmLib/lattices/lattice_indexer.hpp
@@ -16,86 +16,135 @@ namespace hmLib{
 		@sa gap_lattice_indexer
 		*/
 		template<unsigned int dim_>
-		struct lattice_indexer{
+		struct lattice_indexer {
 			friend struct lattice_indexer<dim_ + 1>;
 			using this_type = lattice_indexer<dim_>;
 			using lower_type = lattice_indexer<dim_ - 1>;
-			using point_type = lattices::point<dim_>;
+			using point_type = point<dim_>;
 		public:
-			//!Degree of the lattice dimmension
-			static constexpr unsigned int dim(){ return dim_; }
+			static constexpr unsigned int dim() { return dim_; }
 		public:
-			lattice_indexer() : Size(0), Lower(){}
+			lattice_indexer() : Size(0), Lower() {}
 			template<typename... others>
-			lattice_indexer(size_type  Size_, others... Others) : Size(Size_), Lower(Others...){
+			lattice_indexer(size_type  Size_, others... Others) : Size(Size_), Lower(Others...) {
 				static_assert(sizeof...(others)+1 == dim_, "The argument number is different from the dim number");
 			}
 		public:
+			//!Get number of elements included in the lattice
 			size_type lattice_size()const { return Size*Lower.lattice_size(); }
-			template<typename req_dim_>
+			//!Get the size of req_dim_ dimmension
+			template<unsigned int  req_dim_>
 			size_type size()const {
 				static_assert(req_dim_ < dim_, "requested dim is larger than lattice's dim.");
-				return axis_size_getter<req_dim_>(*this);
+				return dim_solver<req_dim_>().size(*this);
+			}
+			//!Set the size of req_dim_ dimmension
+			template<unsigned int  req_dim_>
+			void resize(size_type Size_) {
+				static_assert(req_dim_ < dim_, "requested dim is larger than lattice's dim.");
+				dim_solver<req_dim_>().resize(*this, Size_);
 			}
 		public:
+			//Get index value from number list with checking over range.
 			template<typename... others>
-			index_type index(index_type Pos_, others... Others)const{
+			index_type index(index_type Pos_, others... Others)const {
 				static_assert(sizeof...(others)+1 == dim_, "The argument number is different from the dim number");
-				hmLib_assert(Pos_ < Size, lattices::out_of_range_access, "Pos is larger than Size.");
-				return Pos_ + Lower.index(Others...)*Lower.lattice_size<0>();
+				return index_calc(0, 1 , Pos_, Others...);
 			}
+			//Get index value from point with checking over range.
+			index_type index(point_type Point_)const {
+				return point_index_calc(0, 1 , 0, Point_);
+			}
+			//Get index value from number list without checking over range.
 			template<typename... others>
-			index_type operator()(index_type Pos_, others... Others)const{
+			index_type operator()(index_type Pos_, others... Others)const {
 				static_assert(sizeof...(others)+1 == dim_, "The argument number is different from the dim number");
-				return Pos_ + Lower(Others...)*Lower.lattice_size<0>();
+				return operator_index_calc(0, 1 , Pos_, Others...);
 			}
-			index_type index(point_type Point_)const{
-				static_assert(sizeof...(others)+1 == dim_, "The argument number is different from the dim number");
-				hmLib_assert(Point_[0] < Size, lattices::out_of_range_access, "Pos is larger than Size.");
-				return Point_[0] + Lower.index(Others...)*Lower.size<0>();
+			//Get index value from point without checking over range.
+			index_type operator()(point_type Point_)const {
+				return point_operator_index_calc(0, 1 , 0, Point_);
 			}
-			template<typename... others>
-			index_type operator()(index_type Pos_, others... Others)const{
-				static_assert(sizeof...(others)+1 == dim_, "The argument number is different from the dim number");
-				return Pos_ + Lower(Others...)*Lower.lattice_size<0>();
-			}
-			std::pair<index_type, index_type> index_range()const{ return std::pair<index_type,index_type>(0, lattice_size()); }
+			//Get minimum and maximum index value of the lattice.
+			std::pair<index_type, index_type> index_range()const { return index_range_calc(0, 0, 1); }
 		private:
-			lower_type Lower;
 			size_type Size;
+			lower_type Lower;
 		private:
-			template<unsigned int point_dim_>
-			index_type checked_index(const point<point_dim_>& Point_)const{
-				hmLib_assert(Point_[point_dim_ - dim] < Size, lattices::out_of_range_access, "Pos is larger than Size.");
-				return Point_[point_dim_ - dim] + Lower.cheked_index(Point_)*Lower.size<0>();
+			std::pair<index_type, index_type> index_range_calc(index_type Min_, index_type Max_, diff_type Step_)const {
+				if (Step_ < 0) {
+					Min_ += Step_ * (Size - 1);
+				} else {
+					Max_ += Step_ * (Size - 1);
+				}
+
+				return Lower.index_range_calc(Min_, Max_, Step_ * Size);
+			}
+			template<typename... others>
+			index_type index_calc(index_type Sum_, diff_type Step_, index_type Pos_, others... Others)const {
+				hmLib_assert(0 <= Pos_ && static_cast<size_type>(Pos_) < Size, lattices::out_of_range_access, "Pos is larger than Size.");
+				return Lower.index_calc(Sum_ + Pos_ * Step_, Step_*Size, Others...);
+			}
+			template<typename point_t>
+			index_type point_index_calc(index_type Sum_, diff_type Step_, index_type No_, const point_t& Point_)const {
+				hmLib_assert(0 <= Point_[No_] && static_cast<size_type>(Point_[No_]) < Size, lattices::out_of_range_access, "Pos is larger than Size.");
+				return Lower.point_index_calc(Sum_ + Point_[No_] * Step_, Step_*Size, No_ + 1, Point_);
+			}
+			template<typename... others>
+			index_type operator_index_calc(index_type Sum_, diff_type Step_, index_type Pos_, others... Others)const {
+				return Lower.operator_index_calc(Sum_ + Pos_ * Step_, Step_*Size, Others...);
+			}
+			template<typename point_t>
+			index_type point_operator_index_calc(index_type Sum_, diff_type Step_, index_type No_, const point_t& Point_)const {
+				return Lower.point_operator_index_calc(Sum_ + Point_[No_] * Step_, Step_*Size, No_ + 1, Point_);
 			}
 			template<unsigned int req_dim_, typename T = void>
-			struct axis_size_getter{
-				size_type operator()(const this_type& This){ return This.Lower.size<req_dim_ - 1>(); }
+			struct dim_solver {
+				size_type size(const this_type& This) { return This.Lower.size<req_dim_ - 1>(); }
+				void resize(this_type& This, size_type Size_) { This.Lower.resize<req_dim_ - 1>(Size_); }
 			};
 			template<typename T>
-			struct axis_size_getter<0, T>{
-				size_type operator()(const this_type& This){ return This.Size; }
+			struct dim_solver<0, T> {
+				size_type size(const this_type& This) { return This.Size; }
+				void resize(this_type& This, size_type Size_) { This.Size = Size_; }
 			};
 		};
 		template<>
-		struct lattice_indexer<0>{
+		struct lattice_indexer<0> {
+			friend struct lattice_indexer<1>;
 			using this_type = lattice_indexer<0>;
 		public:
 			lattice_indexer() = default;
 			template<typename... others>
-			lattice_indexer(others... Others){
-				static_assert(sizeof...(Others) == 0, "Argument for lattice_indexer::lattice_indexer is not enough");
+			index_type index(index_type Pos_, others... Others)const {
+				static_assert(sizeof...(Others) == 0, "Argument for lattice_indexer::index is not enough");
+				return operator()(Pos_, Others...);
 			}
-			index_type index()const{
-				return 0;
+			template<typename... others>
+			index_type operator()(index_type Pos_, others... Others)const { return 0; }
+			size_type lattice_size()const { return 1; }
+			template<unsigned int req_dim_>
+			size_type size()const {
+				return 1;
 			}
-			index_type operator()(void)const{
-				return 0;
+		private:
+			std::pair<index_type, index_type> index_range_calc(index_type Min_, index_type Max_, diff_type Step_)const {
+				return std::pair<index_type, size_type>(Min_, Max_);
 			}
-			size_type lattice_size()const{ return 1; }
-			template<typename req_dim_>
-			size_type lattice_size()const{ return 1; }
+			index_type index_calc(index_type Sum_, diff_type Step_)const {
+				return Sum_;
+			}
+			index_type operator_index_calc(index_type Sum_, diff_type Step_)const {
+				return Sum_;
+			}
+			template<typename point_t>
+			index_type point_index_calc(index_type Sum_, diff_type Step_, index_type No_, const point_t& Point_)const {
+				return Sum_;
+			}
+			template<typename point_t>
+			index_type point_operator_index_calc(index_type Sum_, diff_type Step_, index_type No_, const point_t& Point_)const {
+				return Sum_;
+			}
 		};
 	}
 }
