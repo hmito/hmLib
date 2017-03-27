31211e415cb2e9803ee87833bb28f0e7c54f5b9f
hmito <hmito@outlook.com>
2017-03-27 20:33:22 +0100

    Add index_to_point.

--- hmLib/lattices/utility.hpp
+++ hmLib/lattices/utility.hpp
@@ -14,12 +14,17 @@ namespace hmLib{
 		using point = euclidean::point<index_type, dim_>;
 
 		template<typename ...others>
-		auto make_point(others... Others)->point<sizeof...(others)>{
-			return point<sizeof...(others)>{Others...};
-		}
+		auto make_point(others... Others)->point<sizeof...(others)>{return point<sizeof...(others)>{Others...};}
+
 		template<unsigned int dim_>
-		point<dim_> seqno_to_point(diff_type SecNo, point<dim_> Size) {
-			//for  iterator
+		point<dim_> index_to_point(diff_type Index, const point<dim_>& Size) {
+			point<dim_> Pos;
+			for (unsigned int no = 0; no < dim_;++no) {
+				Pos[no] = (Index%Size[no]);
+				Index /= Size[no];
+			}
+
+			return Pos;
 		}
 	}
 }
