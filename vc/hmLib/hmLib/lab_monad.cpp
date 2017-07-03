5adb6bb1d326ac89eec186c4b772ed1194179adf
hmito <hmito@outlook.com>
2017-07-04 00:31:36 +0100

    Add lazy_evaluator and remove apply_to_own

--- vc/hmLib/hmLib/lab_monad.cpp
+++ vc/hmLib/hmLib/lab_monad.cpp
@@ -5,6 +5,7 @@
 #include<type_traits>
 #include<iostream>
 #include<boost/optional.hpp>
+#include<cmath>
 
 //monad
 namespace hmLib {
@@ -62,7 +63,7 @@ namespace hmLib {
 		struct is_same_monad{
 		private:
 			template<typename V, typename W>
-			static auto check(V,W)->std::bool_constant<is_monad<V>::value && std::is_same<W, typename monad_traits<V>::rebind_t<typename monad_traits<W>::value_type>>::value>;
+			static auto check(V,W)->std::bool_constant<is_monad<V>::value && std::is_same<W, typename monad_traits<V>::template rebind_t<typename monad_traits<W>::value_type>>::value>;
 			static auto check(...)->std::false_type;
 		public:
 			using type = decltype(check(std::declval<T>(), std::declval<U>()));
@@ -81,34 +82,7 @@ namespace hmLib {
 			using type = typename monadic_base<typename monad_traits<T>::value_type>::type;
 		};
 
-		namespace detail{
-			template<typename T, bool IsMonad = is_monad<T>::value>
-			struct apply_to_own_impl{
-				template<typename fn>
-				void operator()(fn&& Fn, T& val){ val = std::move(Fn(std::move(val))); }
-			};
-			template<typename T>
-			struct apply_to_own_impl<T, true>{
-				template<typename fn>
-				void operator()(fn&& Fn, T& m){ m.apply_to_own(Fn); }
-			};
-		}
-		template<typename fn, typename T>
-		void apply_to_own(fn&& Func, T& val){
-			detail::apply_to_own_impl<T>()(std::forward<fn>(Func), val);
-		}
-
 		namespace detail {
-			template<typename fn, typename monad, bool CanApplyToOwn = std::is_same<monadic_base<fn>::type, result_of<fn(monadic_base<fn>::type)>::type>::value>
-			struct apply_to_monad_impl {
-				auto operator()(fn& Fn, const monad& m) { return m.apply(Fn); }
-				auto operator()(fn& Fn, monad&& m) { return m.apply(Fn); }
-			};
-			template<typename fn, typename monad>
-			struct apply_to_monad_impl<fn, monad, true> {
-				auto operator()(fn& Fn, const monad& m) { return m.apply(Fn); }
-				auto operator()(fn& Fn, monad&& m) { return std::move(m.apply_to_own(Fn)); }
-			};
 			template<typename T, bool IsMonad = is_monad<T>::value>
 			struct apply_impl {
 				template<typename fn>
@@ -119,9 +93,9 @@ namespace hmLib {
 			template<typename T>
 			struct apply_impl<T, true> {
 				template<typename fn>
-				auto operator()(fn&& Fn, const T& val) { return apply_to_monad_impl<fn, T>()(std::forward<fn>(Fn), val); }
+				auto operator()(fn&& Fn, const T& val) { return val.apply(Fn); }
 				template<typename fn>
-				auto operator()(fn&& Fn, T&& val) { return std::move(apply_to_monad_impl<fn, T>()(std::forward<fn>(Fn), std::move(val))); }
+				auto operator()(fn&& Fn, T&& val) { return val.apply(Fn); }
 			};
 		}
 		template<typename fn, typename T>
@@ -132,8 +106,8 @@ namespace hmLib {
 		namespace detail{
 			template<typename monad, typename value_type, bool IsNested = is_same_monad<monad, value_type>::value && is_omitable_monad<monad>::value>
 			struct wrap_impl {
-				auto operator()(value_type&& v) { return typename monad_traits<monad>::rebind_t<value_type>(std::move(v)); }
-				auto operator()(const value_type& v) { return typename monad_traits<monad>::rebind_t<value_type>(v); }
+				auto operator()(value_type&& v) { return typename monad_traits<monad>::template rebind_t<value_type>(std::move(v)); }
+				auto operator()(const value_type& v) { return typename monad_traits<monad>::template rebind_t<value_type>(v); }
 			};
 			template<typename monad, typename value_type>
 			struct wrap_impl<monad, value_type, true> {
@@ -176,8 +150,40 @@ namespace hmLib {
 			return serialize_impl<monad, value_type>(std::forward<monad>(m));
 		}
 
+		struct evaluate_t {}static const eval;
+
+		template<typename monad, typename fn>
+		struct lazy_evaluator {
+		private:
+			using this_type = lazy_evaluator<monad, fn>;
+			using arg_type = typename monadic_base<monad>::type;
+		private:
+			monad M;
+			fn Fn;
+		public:
+			lazy_evaluator(monad M_, fn Fn_):M(std::move(M_)),Fn(std::move(Fn_)){}
+			auto evaluate() { return std::move(M.apply(Fn)); }
+			friend auto operator >> (const this_type& This, evaluate_t) {
+				return std::move(This.evaluate());
+			}
+			friend auto operator >> (this_type&& This, evaluate_t) {
+				return std::move(This.evaluate());
+			}
+			template<typename nfn>
+			friend auto operator >> (const this_type& This, const nfn& NFn) {
+				return make_lazy_evaluator(This.M, [Fn, NFn](arg_type v)->decltype(auto) {return NFn(Fn(v)); });
+			}
+			template<typename nfn>
+			friend auto operator >> (this_type&& This, const nfn& NFn) {
+				return make_lazy_evaluator(std::move(This.M), [Fn=This.Fn, NFn](arg_type v)->decltype(auto) {return NFn(Fn(v)); });
+			}
+		};
+		template<typename monad,typename fn>
+		auto make_lazy_evaluator(monad&& M, fn&& Fn) { return lazy_evaluator<typename remove_cvref<monad>::type, typename remove_cvref<fn>::type>(std::forward<monad>(M),std::forward<fn>(Fn)); }
+
+
 		template<typename T>
-		struct identity{
+		struct identity {
 			using monad_category = monad_categories::omitable_monad_tag;
 			using value_type = T;
 		private:
@@ -190,30 +196,30 @@ namespace hmLib {
 		private:
 			T val;
 		public:
-/*			identity() = default;
+			/*			identity() = default;
 			identity(const this_type&) = default;
 			this_type& operator=(const this_type&) = default;
 			identity(this_type&&)= default;
 			this_type& operator=(this_type&&) = default;*/
-			identity():val(){
+			identity() :val() {
 				std::cout << "construct" << std::endl;
 			}
-			identity(const this_type& other):val(other.val) {
-				std::cout << "construct with copy" << std::endl;
+			identity(const this_type& other) :val(other.val) {
+				std::cout << "construct with copy\t" <<val<< std::endl;
 			}
-			this_type& operator=(const this_type& other){
-				std::cout << "copy" << std::endl;
-				if(this != &other){
+			this_type& operator=(const this_type& other) {
+				std::cout << "copy\t" << other.val << std::endl;
+				if (this != &other) {
 					val = other.val;
 				}
 				return *this;
 			}
-			identity(this_type&& other) noexcept:val(std::move(val)) {
-				std::cout << "construct with move" << std::endl;
+			identity(this_type&& other) noexcept:val(std::move(other.val)) {
+				std::cout << "construct with move\t" << val << std::endl;
 			}
-			this_type& operator=(this_type&& other)noexcept{
-				std::cout << "move" << std::endl;
-				if(this != &other){
+			this_type& operator=(this_type&& other)noexcept {
+				std::cout << "move\t" << other.val<<std::endl;
+				if (this != &other) {
 					val = std::move(other.val);
 				}
 				return *this;
@@ -221,39 +227,32 @@ namespace hmLib {
 
 		public:
 			//for monad
-			identity(T val_):val(std::move(val_)){
-				std::cout << "construct with value" << std::endl;
+			identity(T val_) :val(std::move(val_)) {
+				std::cout << "construct with value\t" << val<<std::endl;
 			}
 			//for omitable_monad
-			identity(identity<identity<T>> m_) :val(std::move(m_.val.val)){
-				std::cout << "construct with omit" << std::endl;
+			identity(identity<identity<T>> m_) :val(std::move(m_.val.val)) {
+				std::cout << "construct with omit\t" << val<<std::endl;
 			}
 		public:
 			//for monad
 			template<typename fn>
-			auto apply(fn&& Func)const{
+			auto apply(fn&& Func)const {
 				std::cout << "\tapply" << std::endl;
 				return wrap<this_type>(hmLib::functional::apply(Func, val));
 			}
-			//for monad
-			template<typename fn>
-			this_type& apply_to_own(fn&& Func){
-				std::cout << "\tapply_to_own" << std::endl;
-				hmLib::functional::apply_to_own(Func, val);
-				return *this;
-			}
 		public:
 			//for monad
 			template<typename fn>
-			friend auto operator >> (this_type&& This, fn&& Func){
+			friend auto operator >> (this_type&& This, fn&& Func) {
 				std::cout << "\tmove operator>>\n";
-				return std::move( hmLib::functional::apply(std::forward<fn>(Func), std::move(This)) );
+				return make_lazy_evaluator(std::move(This),std::forward<fn>(Func));
 			}
 			//for monad
 			template<typename fn>
-			friend auto operator>>(const this_type& This, fn&& Func) {
+			friend auto operator >> (const this_type& This, fn&& Func) {
 				std::cout << "\tcopy operator>>\n";
-				return hmLib::functional::apply(std::forward<fn>(Func), This);
+				return make_lazy_evaluator(This, std::forward<fn>(Func));
 			}
 		public:
 			T& get() { return val; }
@@ -263,12 +262,20 @@ namespace hmLib {
 	}
 }
 
+int ftoi(double f) { return static_cast<int>(f * 10); }
+double itod(int i) { return 1.0 / i; }
+double mysin(double v) { return std::sin(v); }
+auto ans = itod(ftoi(5.5f));
+
+//auto Ans = identity<float>(5.5) >> ftoi >> itod >> eval;
+
 struct hoge{
 	hoge() = default;
 	hoge(const hoge&) = delete;
 	hoge& operator=(const hoge&) = delete;
 	hoge(hoge&&) = default;
 	hoge& operator=(hoge&&) = default;
+	friend std::ostream& operator << (std::ostream& out, const hoge&h) { return out; }
 };
 int main() {
 	using namespace hmLib::functional;
@@ -283,7 +290,14 @@ int main() {
 	std::cout << std::is_same <identity<double>, identity<int>>::value << std::endl;
 
 	std::cout << "=== for b ===" << std::endl;
-	auto b = identity<double>(3.4) >> [](double v)->double{return v * 2; };
+	auto b = identity<double>(3.4) 
+		>> [](double v)->double {return v * 2; } 
+		>> [](double v)->int {return static_cast<int>(v * 10); } 
+		>> [](double v)->double{return std::sqrt(v); } 
+		>> mysin
+		>> ftoi
+		>> eval;
+
 	std::cout << typeid(b).name() << std::endl;
 	std::cout << b.get()<<std::endl;
 
