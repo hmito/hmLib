938119fa95e11fefb5c4cd2e9df56533f6f01ae9
hmito <hmito@outlook.com>
2017-07-14 19:22:22 +0100

    Fix: max_return_element contains bugs.

--- hmLib/optimize/value_optimize.hpp
+++ hmLib/optimize/value_optimize.hpp
@@ -5,8 +5,10 @@ namespace hmLib{
 	namespace optimize{
 		template<typename input_iterator, typename evaluator>
 		input_iterator max_return_element(input_iterator Beg, input_iterator End, evaluator&& Eval){
-			input_iterator MaxItr;
+			if(Beg == End)return End;
+
 			auto Max = Eval(*Beg);
+			input_iterator MaxItr = Beg;
 			for(++Beg; Beg != End; ++Beg){
 				auto Val = Eval(*Beg);
 				if(Val > Max){
