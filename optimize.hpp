b12b344daf672e3e35be58f1c686296324d8bde6
hmito <hmito@outlook.com>
2017-08-22 20:10:53 +0100

    Add golden section search in optimize.hpp

--- optimize.hpp
+++ optimize.hpp
@@ -1,5 +1,5 @@
 #ifndef HMLIB_OPTIMIZE_INC
-#define HMLIB_OPTIMIZE_INC 100
+#define HMLIB_OPTIMIZE_INC 101
 #
 #include<random>
 #include<utility>
@@ -82,9 +82,177 @@ namespace hmLib{
 			};
 		}
 
+		/*!
+		@brief	golden section search method
+		Search the maximum value of Evaluate in the state [MinVal:MaxVal].
+		@param [in]	Evaluate	Target function. The function need not to have a range where the slope becomes zero.
+		@param [in]	MinVal	Minimum value of searching range.
+		@param [in]	MaxVal	Maximum value of searching range.
+		@param [in]	Error	Accuracy of the search. The return range will be less than Error.
+		@return	Pair of the state (Min, Max) where Evaluate becomes maximum value.
+				The distance of return pair Max-Min will be less than Error if the searching is sucessfully finished.
+				If the searching of maximum value is failed, the return pair distance will be larger than Error.*/
+		template<typename evaluate, typename state>
+		std::pair<state, state> golden_section_search(evaluate&& Evaluate, state MinVal, state MaxVal, double Error) {
+			constexpr double gratio = 1.6180339887498948482045868343656;
+
+			auto MinEval = Evaluate(MinVal);
+			auto MaxEval = Evaluate(MaxVal);
+
+			//First division: MinSide
+			auto LowerVal = (gratio*MinVal + MaxVal) / (1 + gratio);
+			auto LowerEval = Evaluate(LowerVal);
+			if (LowerEval < MinEval && LowerEval < MaxEval)return std::make_pair(MinVal, MaxVal);
+
+			//Second division: MaxSide
+			state UpperVal = (gratio*LowerVal + MaxVal) / (1 + gratio);
+			auto UpperEval = Evaluate(UpperVal);
+
+			while (MaxVal - MinVal > Error) {
+				//Update Min 
+				if (LowerEval < UpperEval) {
+					MinVal = std::move(LowerVal);
+					MinEval = std::move(LowerEval);
+
+					LowerVal = std::move(UpperVal);
+					LowerEval = std::move(UpperEval);
+
+					UpperVal = (gratio*LowerVal + MaxVal) / (1 + gratio);
+					UpperEval = Evaluate(UpperVal);
+				}
+				//Update Max
+				else{
+					MaxVal = std::move(UpperVal);
+					MaxEval = std::move(UpperEval);
+
+					UpperVal = std::move(LowerVal);
+					UpperEval = std::move(LowerEval);
+
+					LowerVal = (MinVal + gratio*UpperVal) / (1 + gratio);
+					LowerEval = Evaluate(LowerVal);
+				}
+			}
+
+			return std::make_pair(MinVal, MaxVal);
+		}
+
+		/*!
+		@brief	golden section search method with small division method.
+		Search the maximum value of Evaluate in the state [MinVal:MaxVal].
+		@param [in]	Evaluate	Target function. The function can have a range where the slope becomes zero.
+		@param [in]	MinVal	Minimum value of searching range.
+		@param [in]	MaxVal	Maximum value of searching range.
+		@param [in]	Error	Accuracy of the search. The return range will be less than Error.
+		@return	Pair of the state (Min, Max) where Evaluate becomes maximum value.
+				The distance of return pair Max-Min will be less than Error if the searching is sucessfully finished.
+				If the searching of maximum value is failed, the return pair distance will be larger than Error.*/
+		template<typename evaluate, typename state>
+		std::pair<state, state> flatable_golden_section_search(evaluate&& Evaluate, state MinVal, state MaxVal, double Error) {
+			constexpr double gratio = 1.6180339887498948482045868343656;
+
+			auto MinEval = Evaluate(MinVal);
+			auto MaxEval = Evaluate(MaxVal);
+
+			//First division: MinSide
+			auto LowerVal = (gratio*MinVal + MaxVal) / (1 + gratio);
+			auto LowerEval = Evaluate(LowerVal);
+			if (LowerEval < MinEval && LowerEval < MaxEval)return std::make_pair(MinVal, MaxVal);
+
+			//Second division: MaxSide
+			state UpperVal = (gratio*LowerVal + MaxVal) / (1 + gratio);
+			auto UpperEval = Evaluate(UpperVal);
+
+			while (MaxVal - MinVal > Error) {
+				//Update Min 
+				if (LowerEval < UpperEval) {
+					MinVal = std::move(LowerVal);
+					MinEval = std::move(LowerEval);
+
+					LowerVal = std::move(UpperVal);
+					LowerEval = std::move(UpperEval);
+
+					UpperVal = (gratio*LowerVal + MaxVal) / (1 + gratio);
+					UpperEval = Evaluate(UpperVal);
+				}
+				//Update Max
+				else if (UpperEval < LowerEval) {
+					MaxVal = std::move(UpperVal);
+					MaxEval = std::move(UpperEval);
+
+					UpperVal = std::move(LowerVal);
+					UpperEval = std::move(LowerEval);
+
+					LowerVal = (MinVal + gratio*UpperVal) / (1 + gratio);
+					LowerEval = Evaluate(LowerVal);
+				}
+				//Same Case
+				else {
+					unsigned int Div = 11;
+
+					unsigned int BestNoLower;
+					unsigned int BestNoUpper;
+
+					while (true) {
+						BestNoLower = 0;
+						BestNoUpper = 0;
+						auto BestEval = MinEval;
+						for (unsigned int i = 1; i < Div - 1; ++i) {
+							state Val = (MinVal*(Div - i) + MaxVal*i) / Div;
+							auto Eval = Evaluate(Val);
+
+							if (BestEval < Eval) {
+								BestNoLower = i;
+								BestNoUpper = i;
+								BestEval = Eval;
+							} else if (BestEval == Eval) {
+								BestNoUpper = i;
+							}
+						}
+						if (BestEval < MaxEval) {
+							BestNoLower = Div;
+							BestNoUpper = Div;
+							BestEval = MaxEval;
+						} else if (BestEval == MaxEval) {
+							BestNoUpper = Div;
+						}
+
+						if (BestNoLower != 0 || BestNoUpper != Div)break;
+
+						if ((MaxVal - MinVal) / Div < Error) {
+							return std::make_pair(MinVal, MaxVal);
+						}
+						Div = (Div - 1) * 10 + 1;
+					}
+					if (BestNoLower > 0)BestNoLower -= 1;
+					if (BestNoUpper < Div)BestNoUpper += 1;
+					auto NewMinVal = (MinVal*(Div - BestNoLower) + MaxVal*BestNoLower) / Div;
+					auto NewMaxVal = (MinVal*(Div - BestNoUpper) + MaxVal*BestNoUpper) / Div;
+
+					if (NewMinVal <= MinVal && NewMaxVal >= MaxVal) {
+						return std::make_pair(MinVal, MaxVal);
+					}
+					MinVal = NewMinVal;
+					MaxVal = NewMaxVal;
+					MinEval = Evaluate(MinVal);
+					MaxEval = Evaluate(MaxVal);
+
+					//First division: MinSide
+					auto LowerVal = (gratio*MinVal + MaxVal) / (1 + gratio);
+					auto LowerEval = Evaluate(LowerVal);
+					if (LowerEval < MinEval && LowerEval < MaxEval)return std::make_pair(MinVal, MaxVal);
+
+					//Second division: MaxSide
+					state UpperVal = (gratio*LowerVal + MaxVal) / (1 + gratio);
+					auto UpperEval = Evaluate(UpperVal);
+				}
+			}
+
+			return std::make_pair(MinVal, MaxVal);
+		}
+		
 		//hill climing search
-		template<typename state, typename evaluate, typename mutate, typename urbg, typename state_breaker>
-		auto hill_climbing_search(state& State, evaluate&& Evaluate, mutate&& Mutate, state_breaker&& Breaker, urbg&& URBG)->decltype(Evaluate(State)){
+		template<typename evaluate, typename state, typename mutate, typename urbg, typename state_breaker>
+		auto hill_climbing_search(evaluate&& Evaluate, state& State, mutate&& Mutate, state_breaker&& Breaker, urbg&& URBG)->decltype(Evaluate(State)){
 			auto Eval = Evaluate(State);
 			state NState = State;
 
@@ -104,8 +272,8 @@ namespace hmLib{
 		}
 
 		//simulated annealing search
-		template<typename state, typename evaluate, typename mutate, typename temp, typename urbg, typename state_breaker>
-		auto simulated_annealing_search(state& State, evaluate&& Evaluate, mutate&& Mutate, temp&& Temp, state_breaker&& Breaker, unsigned int StepNum, urbg&& URBG)->decltype(Evaluate(State)){
+		template<typename evaluate, typename state, typename mutate, typename temp, typename urbg, typename state_breaker>
+		auto simulated_annealing_search(evaluate&& Evaluate, state& State, mutate&& Mutate, temp&& Temp, state_breaker&& Breaker, unsigned int StepNum, urbg&& URBG)->decltype(Evaluate(State)){
 			auto Eval = Evaluate(State);
 			state NState = State;
 
@@ -128,8 +296,8 @@ namespace hmLib{
 		}
 
 		//brute-forece search
-		template<typename state, typename evaluate, typename state_stepper>
-		auto brute_force_search(state& State, evaluate&& Evaluate, state_stepper&& StateStepper)->decltype(Evaluate(State)){
+		template<typename evaluate, typename state, typename state_stepper>
+		auto brute_force_search(evaluate&& Evaluate, state& State, state_stepper&& StateStepper)->decltype(Evaluate(State)){
 			StateStepper.initialize(State);
 
 			auto Eval = Evaluate(State);
