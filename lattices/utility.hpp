984a7ae84427acdf16ae73e7c87532a6701e72c2
hmito <hmito@outlook.com>
2017-08-24 00:07:49 +0100

    Fix: the include file was

--- lattices/utility.hpp
+++ lattices/utility.hpp
@@ -47,14 +47,14 @@ namespace hmLib{
 		point<dim_> make_torus_point(const point<dim_>& Point, const point<dim_>& Size){
 			point<dim_> Ans = Point;
 			for(unsigned int i = 0; i < dim_; ++i){
-				Ans[i] = algorithm::positive_mod(Ans[i], Size[i]);
+				Ans[i] = positive_mod(Ans[i], Size[i]);
 			}
 			return Ans;
 		}
 		template<unsigned int dim_>
 		void torus_point(point<dim_>& Point, const point<dim_>& Size){
 			for(unsigned int i = 0; i < dim_; ++i){
-				Point[i] = algorithm::positive_mod(Point[i], Size[i]);
+				Point[i] = positive_mod(Point[i], Size[i]);
 			}
 		}
 	}
