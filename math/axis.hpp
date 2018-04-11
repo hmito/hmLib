7f93f2477d97d5461b2f6916b03623f4a9796104
Koichi Ito <hmito@outlook.com>
2018-04-10 18:13:18 -0700

    Add threshold values in axis

--- math/axis.hpp
+++ math/axis.hpp
@@ -13,10 +13,10 @@ namespace hmLib {
 			constexpr struct floor_grid_tag {} floor_grid;
 			constexpr struct ceil_grid_tag {} ceil_grid;
 			namespace detail {
-				template<typename grid_policy>
+				template<typename grid_policy, int log10_index_threshold>
 				struct grid_adjuster {};
-				template<>
-				struct grid_adjuster<round_grid_tag> {
+				template<int log10_index_threshold>
+				struct grid_adjuster<round_grid_tag, log10_index_threshold> {
 					template<typename index_type>
 					static index_type index_cast(double index_) { return static_cast<index_type>(std::round(index_)); }
 					template<typename index_type>
@@ -24,19 +24,21 @@ namespace hmLib {
 					template<typename value_type, typename difference_type>
 					static std::pair<value_type, value_type> value_range(value_type Value, difference_type Interval) { return std::pair<value_type, value_type>(Value-Interval/2.0, Value+Interval/2.0); }
 				};
-				template<>
-				struct grid_adjuster<floor_grid_tag> {
+				template<int log10_index_threshold>
+				struct grid_adjuster<floor_grid_tag, log10_index_threshold> {
+					inline double index_threshold() { return std::pow(10, log10_index_threshold); }
 					template<typename index_type>
-					static index_type index_cast(double index_) { return static_cast<index_type>(std::floor(index_)); }
+					static index_type index_cast(double index_) { return static_cast<index_type>(std::floor(index_+index_threshold())); }
 					template<typename index_type>
 					static std::pair<double, double> index_range(index_type Index) { return std::pair<double, double>(Index+0.0, Index+1.0); }
 					template<typename value_type, typename difference_type>
 					static std::pair<value_type, value_type> value_range(value_type Value, difference_type Interval) { return std::pair<value_type, value_type>(Value, Value+Interval); }
 				};
-				template<>
-				struct grid_adjuster<ceil_grid_tag> {
+				template<int log10_index_threshold>
+				struct grid_adjuster<ceil_grid_tag, log10_index_threshold> {
+					inline double index_threshold() { return std::pow(10, log10_index_threshold); }
 					template<typename index_type>
-					static index_type index_cast(double index_) { return static_cast<index_type>(std::ceil(index_)); }
+					static index_type index_cast(double index_) { return static_cast<index_type>(std::ceil(index_-index_threshold())); }
 					template<typename index_type>
 					static std::pair<double, double> index_range(index_type Index) { return std::pair<double, double>(Index-1.0, Index+0.0); }
 					template<typename value_type, typename difference_type>
@@ -44,12 +46,14 @@ namespace hmLib {
 				};
 			}
 		}
-		template<typename index_type>
+		template<typename index_type, int log10_index_threshold = -8>
 		struct weighted_index_range {
 			using weighted_index = std::pair<index_type, double>;
 		private:
+			inline double index_threshold() { return std::pow(10, log10_index_threshold); }
+		private:
 			index_type LowerIndex;
-			index_type UpperIndex;
+			index_type EndIndex;
 			double LowerProb;
 			double UpperProb;
 			double Weidth;
@@ -63,12 +67,27 @@ namespace hmLib {
 				using iterator_category = std::input_iterator_tag;
 			private:
 				index_type Index;
-				const weighted_index_range* pRange;
+				index_type LowerIndex;
+				index_type EndIndex;
+				double LowerProb;
+				double UpperProb;
 			public:
 				iterator() = default;
-				iterator(index_type Index_, const weighted_index_range& Range_):Index(Index_), pRange(&Range_) {}
-				value_type operator*()const { return (*pRange)[Index]; }
-				//				const value_type* operator->()const { return &(*pRange)[Index]; }
+				iterator(index_type Index_, index_type LowerIndex_, index_type EndIndex_, double LowerProb_, double UpperProb_)
+					: Index(Index_) 
+					, LowerIndex(LowerIndex_)
+					, EndIndex(EndIndex_)
+					, LowerProb(LowerProb_)
+					, UpperProb(UpperProb_){
+				}
+				value_type operator*()const {
+					if(Index==0) {
+						return weighted_index(LowerIndex, LowerProb);
+					} else if(LowerIndex+Index==EndIndex-1) {
+						return weighted_index(EndIndex-1, UpperProb);
+					}
+					return weighted_index(Index+LowerIndex, 1.0);
+				}
 				iterator& operator++() { ++Index; return *this; }
 				iterator operator++(int) {
 					iterator Prev = *this;
@@ -83,25 +102,57 @@ namespace hmLib {
 				}
 			};
 		public:
-			weighted_index_range(index_type Index_, double Weidth_)
+			weighted_index_range()noexcept
+				: LowerIndex(0)
+				, EndIndex(0)
+				, LowerProb(0.0)
+				, UpperProb(0.0)
+				, Weidth(0.0) {
+			}
+			weighted_index_range(index_type Index_, double Weidth_)noexcept
 				: LowerIndex(Index_)
-				, UpperIndex(Index_)
+				, EndIndex(Index_+1)
 				, LowerProb(1.0)
 				, UpperProb(0.0)
 				, Weidth(Weidth_) {
 			}
-			weighted_index_range(index_type LowerIndex_, index_type UpperIndex_, double LowerProb_, double UpperProb_, double Weidth_)
+			weighted_index_range(index_type LowerIndex_, index_type UpperIndex_, double LowerProb_, double UpperProb_, double Weidth_)noexcept
 				: LowerIndex(LowerIndex_)
-				, UpperIndex(UpperIndex_)
+				, EndIndex(UpperIndex_+1)
 				, LowerProb(LowerProb_)
 				, UpperProb(UpperProb_)
 				, Weidth(Weidth_) {
+				if(LowerProb<index_threshold()) {
+					if(LowerIndex+2==EndIndex) {
+						if(UpperProb<index_threshold()) {
+							LowerIndex = 0;
+							EndIndex = 0;
+							LowerProb = 0.0;
+							UpperProb = 0.0;
+							Weidth = 0.0;
+						}
+						LowerProb = UpperProb;
+						UpperProb = 0.0;
+						++LowerIndex;
+					} else {
+						LowerProb = 1.0;
+						++LowerIndex;
+					}
+				}else if(UpperProb<index_threshold()) {
+					if(LowerIndex+2==EndIndex) {
+						UpperProb = 0.0;
+						--EndIndex;
+					} else {
+						UpperProb = 1.0;
+						--EndIndex;
+					}
+				}
 			}
 			weighted_index operator[](index_type Index)const {
 				if(Index==0) {
 					return weighted_index(LowerIndex, LowerProb);
-				} else if(LowerIndex+Index==UpperIndex) {
-					return weighted_index(UpperIndex, UpperProb);
+				} else if(LowerIndex+Index==EndIndex-1) {
+					return weighted_index(EndIndex-1, UpperProb);
 				}
 				return weighted_index(Index+LowerIndex, 1.0);
 			}
@@ -109,31 +160,32 @@ namespace hmLib {
 				hmLib_assert(0<=Index && Index < size(), hmLib::access_exceptions::out_of_range_access, "Out of range access.");
 				return operator[](Index);
 			}
-			bool empty()const { return LowerIndex>UpperIndex; }
-			unsigned int size()const { return UpperIndex-LowerIndex+1; }
+			bool empty()const { return LowerIndex==EndIndex; }
+			unsigned int size()const { return EndIndex-LowerIndex; }
 			double weidth()const { return Weidth; }
 			iterator begin()const {
-				return iterator(0, *this);
+				return iterator(0, LowerIndex, EndIndex, LowerProb, UpperProb);
 			}
 			iterator end()const {
-				return iterator(size(), *this);
+				return iterator(size(), LowerIndex, EndIndex, LowerProb, UpperProb);
 			}
 			iterator cbegin()const { return begin(); }
 			iterator cend()const { return end(); }
 		};
 	}
-	template<typename T, typename grid_policy_ = math::grid_policy::round_grid_tag, typename index_type_ = unsigned int, typename calc_type_ = double>
+	template<typename T, typename grid_policy_ = math::grid_policy::round_grid_tag, typename index_type_ = unsigned int, typename calc_type_ = double, int log10_index_threshold_ = -8>
 	struct axis {
 	private:
-		using this_type = axis<T, grid_policy_, index_type_, calc_type_>;
+		using this_type = axis<T, grid_policy_, index_type_, calc_type_, log10_index_threshold_>;
 	public:
 		using value_type = T;
 		using calc_type = calc_type_;
 		using grid_policy = grid_policy_;
 		using difference_type = decltype(std::declval<T>() - std::declval<T>());
 		using index_type = index_type_;
+		static const int log10_index_threshold = log10_index_threshold_;
 	private:
-		using grid_adjuster = math::grid_policy::detail::grid_adjuster<grid_policy>;
+		using grid_adjuster = math::grid_policy::detail::grid_adjuster<grid_policy, log10_index_threshold>;
 	public:
 		struct iterator {
 		public:
@@ -143,18 +195,19 @@ namespace hmLib {
 			using reference = value_type;
 			using iterator_category = std::random_access_iterator_tag;
 		private:
-			const this_type* pAxis;
 			index_type Pos;
+			double a;
+			double b;
 		public:
-			iterator() :pAxis(0), Pos(0) {}
+			iterator()noexcept :Pos(0), a(0.0), b(0.0) {}
 			iterator(const iterator&) = default;
 			iterator& operator=(const iterator&) = default;
 			iterator(iterator&&) = default;
 			iterator& operator=(iterator&&) = default;
-			iterator(const this_type& Axis_, index_type Pos_) : pAxis(&Axis_), Pos(Pos_) {}
+			iterator(index_type Pos_, double a_, double b_): Pos(Pos_), a(a_), b(b_) {}
 		public:
-			value_type operator*()const { return (*pAxis)[Pos]; }
-			value_type operator[](difference_type Val)const { return (*pAxis)[Pos + Val]; }
+			value_type operator*()const { return a*Pos+b; ; }
+			value_type operator[](difference_type Val)const { return a*(Pos+Val)+b; }
 			iterator& operator++() {
 				++Pos; 
 				return *this;
@@ -218,7 +271,7 @@ namespace hmLib {
 		public:
 			index_type index()const { return Pos; }
 		};
-		using weighted_index_range = math::weighted_index_range<index_type>;
+		using weighted_index_range = math::weighted_index_range<index_type, log10_index_threshold>;
 	public:
 		axis():a(0), b(0), Size(0) {}
 		axis(value_type Lower_, value_type Upper_, index_type Size_):a(static_cast<calc_type>(Upper_-Lower_)/(Size_ - 1)), b(Lower_), Size(Size_) {}
@@ -249,8 +302,8 @@ namespace hmLib {
 		}
 		value_type lower()const { return static_cast<value_type>(b); }
 		value_type upper()const { return static_cast<value_type>(a*(size()-1)+b); }
-		iterator begin()const { return iterator(*this, 0); }
-		iterator end()const { return iterator(*this, Size); }
+		iterator begin()const { return iterator(0, a, b); }
+		iterator end()const { return iterator(Size,a,b); }
 		iterator cbegin()const { return begin(); }
 		iterator cend()const { return end(); }
 	public:
@@ -297,16 +350,16 @@ namespace hmLib {
 		index_type Size;
 	};
 
-	template<typename from_grid_policy_, typename to_grid_policy_, typename index_type_ = unsigned int>
+	template<typename from_grid_policy_, typename to_grid_policy_, typename index_type_ = unsigned int, int log10_index_threshold_ = -8>
 	struct axis_mapper {
 	public:
 		using from_grid_policy = from_grid_policy_;
 		using to_grid_policy = to_grid_policy_;
 		using index_type = index_type_;
-		using weighted_index_range = math::weighted_index_range<index_type>;
+		using weighted_index_range = math::weighted_index_range<index_type, log10_index_threshold_>;
 	private:
-		using from_grid_adjuster = math::grid_policy::detail::grid_adjuster<from_grid_policy>;
-		using to_grid_adjuster = math::grid_policy::detail::grid_adjuster<to_grid_policy>;
+		using from_grid_adjuster = math::grid_policy::detail::grid_adjuster<from_grid_policy, log10_index_threshold_>;
+		using to_grid_adjuster = math::grid_policy::detail::grid_adjuster<to_grid_policy, log10_index_threshold_>;
 	private:
 		//to = a*from + b
 		//from = (to - b)/a
@@ -397,7 +450,7 @@ namespace hmLib {
 				Size
 			);
 		case math::make_axis_option::gridfit:
-			using grid_adjuster = math::grid_policy::detail::grid_adjuster<grid_policy>;
+			using grid_adjuster = math::grid_policy::detail::grid_adjuster<grid_policy,-8>;
 
 			Lower -= (Upper-Lower)*grid_adjuster::index_range(0).first / Size;
 			Upper += (Upper-Lower)*grid_adjuster::index_range(0).second / Size;
