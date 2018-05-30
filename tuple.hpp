2c4a8a3de0026a7480cb631381d7d4e438516c86
Koichi Ito <hmito@outlook.com>
2018-05-30 01:35:36 -0700

    Add tuple for each in dynamics version.

--- tuple.hpp
+++ tuple.hpp
@@ -2,24 +2,24 @@
 #define HMLIB_TUPLE_INC 100
 #
 #include<tuple>
+#include<utility>
+#include"exceptions.hpp"
 namespace hmLib {
-	template<std::size_t n, typename Fn, typename... Tuple>
-	auto tuple_apply_at(Fn f, Tuple... t) {
-		return f(std::get<n>(t)...);
-	}
 	namespace detail {
-		template<std::size_t r, std::size_t n, typename Fn, typename... Tuple>
+		template<unsigned int n, unsigned int r>
 		struct tuple_for_each_impl{
-			auto operator()(Fn f, Tuple... t) {
+			template<typename Fn, typename... Tuple>
+			auto operator()(Fn&& f, Tuple&&... t) {
 				return std::tuple_cat(
 					std::make_tuple(f(std::get<n>(t)...)),
-					tuple_for_each_impl<r - 1, n + 1, Fn, Tuple...>()(f, t...)
+					tuple_for_each_impl<n+1,r-1>()(f, t...)
 				);
 			}
 		};
-		template<std::size_t n, typename Fn, typename... Tuple>
-		struct tuple_for_each_impl<1,n,Fn,Tuple...>{
-			auto operator()(Fn f, Tuple... t) {
+		template<unsigned int n>
+		struct tuple_for_each_impl<n,1>{
+			template<typename Fn, typename... Tuple>
+			auto operator()(Fn&& f, Tuple&&... t) {
 				return std::make_tuple(
 					f(std::get<n>(t)...)
 				);
@@ -27,13 +27,45 @@ namespace hmLib {
 		};
 	}
 	template<typename Fn, typename Tuple, typename... OtherTuples>
-	auto tuple_for_each(Fn f, Tuple t, OtherTuples... ot) {
-		return detail::tuple_for_each_impl<std::tuple_size<Tuple>::value, 0, Fn, Tuple, OtherTuples...>()(
+	auto tuple_for_each(Fn&& f, Tuple&& t, OtherTuples&&... ot) {
+		return detail::tuple_for_each_impl<0, std::tuple_size<typename std::decay<Tuple>::type>::value>()(
 			std::forward<Fn>(f),
 			std::forward<Tuple>(t),
 			std::forward<OtherTuples>(ot)...
 		);
 	}
+	namespace detail {
+		template<typename ans_type, unsigned int n, unsigned int r>
+		struct tuple_apply_at_impl {
+			template<typename Fn, typename... Tuple>
+			ans_type operator()(unsigned int pos, Fn&& f, Tuple&&... v) {
+				if(n==pos) {
+					return f(std::get<n>(v)...);
+				}
+				return tuple_apply_at_impl<ans_type, n+1,r-1>()(pos, std::forward<Fn>(f), std::forward<Tuple>(v)...);
+			}
+		};
+		template<typename ans_type, unsigned int n>
+		struct tuple_apply_at_impl<ans_type, n,0>{
+			template<typename Fn, typename... Tuple>
+			ans_type operator()(unsigned int pos, Fn&& f, Tuple&&... v) {
+				hmLib_throw(hmLib::access_exceptions::out_of_range_access, "tuple access is out of range.");
+			}
+		};
+	}
+	template<typename ans_type, typename Fn, typename Tuple, typename... OtherTuples>
+	ans_type tuple_apply_at(unsigned int n, Fn&& f, Tuple&& t, OtherTuples&&... ot){
+		return detail::tuple_apply_at_impl<ans_type, 0,std::tuple_size<typename std::decay<Tuple>::type>::value>()(n,
+			std::forward<Fn>(f),
+			std::forward<Tuple>(t),
+			std::forward<OtherTuples>(ot)...
+		);
+	}
+	template<std::size_t n, typename Fn, typename... Tuple>
+	auto tuple_apply_at(Fn f, Tuple... t) {
+		return f(std::get<n>(t)...);
+	}
+
 }
 #
 #endif
