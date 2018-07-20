2451939797b93306edcc4c082adf29399da451da
Koichi Ito <hmito@outlook.com>
2018-07-20 11:20:18 -0700

    Fix config_vc is applied not only vc++

--- config_vc.h
+++ config_vc.h
@@ -8,13 +8,17 @@ config_vc_v1_01/121204 hmIto
 	拡張子をhppからhに変更
 	インクルードガードマクロの不要なアンダーバーを消去
 */
+#ifdef _MSC_VER
 #	//windows.hのmin,maxを使わせない
-#	define NOMINMAX
+#	ifndef NOMINMAX
+#		define NOMINMAX
+#	endif
 #	//古いunsafeな関数群使用による警告回避
-//#	define _CRT_SECURE_NO_DEPRECATE 1
-#pragma warning( disable:4996 )
-#define _CRT_SECURE_NO_WARNINGS
-#define _AFX_SECURE_NO_WARNINGS
-#define _ATL_SECURE_NO_WARNINGS
-#define _SCL_SECURE_NO_WARNINGS
+#	pragma warning( disable:4996 )
+#	define _CRT_SECURE_NO_WARNINGS
+#	define _AFX_SECURE_NO_WARNINGS
+#	define _ATL_SECURE_NO_WARNINGS
+#	define _SCL_SECURE_NO_WARNINGS
+#endif
+#
 #endif
