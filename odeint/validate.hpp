80ebba76ffb263fa0ab54c0ed0cbb1692de39c29
Koichi Ito <hmito@outlook.com>
2019-04-28 23:55:41 -0700

    Remove interfere and add validate module

---
+++ odeint/validate.hpp
@@ -0,0 +1,8 @@
+#ifndef HMLIB_ODEINT_VALIDATE_INC
+#define HMLIB_ODEINT_VALIDATE_INC 100
+#
+#include"validate/validate_result.hpp"
+#include"validate/state_validate_stepper.hpp"
+#include"validate/step_validate_stepper.hpp"
+#
+#endif
