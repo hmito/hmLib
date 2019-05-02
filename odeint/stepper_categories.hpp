4aac797159b42568f69bc781f7e70ddffe357583
Koichi Ito <hmito@outlook.com>
2019-05-02 15:42:24 -0700

    Add doxygen comments.

--- odeint/stepper_categories.hpp
+++ odeint/stepper_categories.hpp
@@ -4,14 +4,21 @@
 #include<boost/numeric/odeint/stepper/stepper_categories.hpp>
 namespace hmLib {
 	namespace odeint {
+		//! @brief tag for stepper in boost.odeint.
 		using stepper_tag = boost::numeric::odeint::stepper_tag;
+		//! @brief tag for error_stepper in boost.odeint.
 		using error_stepper_tag = boost::numeric::odeint::error_stepper_tag;
+		//! @brief tag for controlled_stepper in boost.odeint.
 		using controlled_stepper_tag = boost::numeric::odeint::controlled_stepper_tag;
+		//! @brief  tag for dense_output_stepper_tag in boost.odeint.
 		using dense_output_stepper_tag = boost::numeric::odeint::dense_output_stepper_tag;
 
+		/*! @brief  tag for adaptive_stepper, which is original stepper in hmLib.
+		adaptive_stepper change time step dt "adaptically" and therefore the given dt might be not used.
+		adaptive_stepper should have a function bool do_step(system, &state, &time, &dt).
+		This function return true if step was not same with given dt.
+		This function change state, time and suggested dt as refrerence argument.*/
 		struct adaptive_stepper_tag {};
-		//bool do_step(system, &state, &time, &dt);
-		//return: true if step was not same with given dt state, time and next suggested dt can be updated.
 	}
 }
 #
