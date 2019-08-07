5768449ee59d1eeec5ec37b0273de6638f799e94
Koichi Ito <hmito@outlook.com>
2019-08-07 16:19:44 +0200

    Fix many bugs in fast_cross_check_line

--- geometry/line.hpp
+++ geometry/line.hpp
@@ -1,5 +1,5 @@
 #ifndef HMLIB_GEOMETRY_LINE_INC
-#define HMLIB_GEOMETRY_LINE_INC
+#define HMLIB_GEOMETRY_LINE_INC 100
 #
 #include<vector>
 #include"point.hpp"
@@ -8,53 +8,48 @@
 namespace hmLib {
 	namespace plane_geometry {
 		template<typename T>
-		using line = std::vector<point<T>>;
-
-		template<typename T>
 		struct fast_cross_check_line {
 		private:
 			using this_type = fast_cross_check_line<T>;
 		public:
-			using const_iterator = line::const_iterator;
-			using const_reverse_iterator = line::const_reverse_iterator;
+			using base_line = std::vector<point<T>>;
+			using rectset = std::vector<rectangle<T>>;
+			using const_iterator = typename base_line::const_iterator;
+			using const_reverse_iterator = typename base_line::const_reverse_iterator;
 		private:
-			line<T> Line;
-			std::vector<rectangle<T>> Rect;
+			base_line Line;
+			rectset Rect;		//size of rectangle including Line[0:Step], Line[Step:2Step]
 			unsigned int Step;
 		public:
-			fast_cross_check_line() = default;
-			fast_cross_check_line(unsigned int Step_):Step(Step_) {}
+			fast_cross_check_line():Step(8) = default;
+			fast_cross_check_line(const this_type&) = default;
+			this_type& operator=(const this_type&) = default;
+			fast_cross_check_line(this_type&&) = default;
+			this_type& operator=(this_type&&) = default;
+			explicit fast_cross_check_line(unsigned int Step_):Step(Step_) {}
+		private:
 			void push_back(point<T> p) {
-				Line.push_back(p);
-
-				if(Rect.empty()) {
+				if (!Rect.empty()) {
 					Rect.back().lower = min_element(Rect.back().lower, p);
 					Rect.back().upper = max_element(Rect.back().upper, p);
-				} else {
-					Rect.back().lower = min_element(Rect.back().lower, p);
-					Rect.back().upper = max_element(Rect.back().upper, p);
-
-					if((Line.size()-1)%Step == 0) {
-						Rect.emplace_back(p, p);
-					}
 				}
+				if (Line.size() % Step == 0) {
+					Rect.emplace_back(p, p);
+				}
+				Line.push_back(p);
 			}
 			template<typename input_iterator>
-			void push_back(input_iterator beg, input_iterator end) {
+			void push_back(input_iterator Beg, input_iterator End) {
 				unsigned int pos = Line.size();
-				Line.insert(Line.end(), beg,end);
+				Line.insert(Line.end(), Beg, End);
 
-				for(; beg!=end; ++beg, ++pos) {
-					if(Rect.empty()) {
-						Rect.back().lower = min_element(Rect.back().lower, *beg);
-						Rect.back().upper = max_element(Rect.back().upper, *beg);
-					} else {
-						Rect.back().lower = min_element(Rect.back().lower, *beg);
-						Rect.back().upper = max_element(Rect.back().upper, *beg);
-
-						if(pos%Step == 0) {
-							Rect.emplace_back(*beg, *beg);
-						}
+				for(; Beg!=End; ++Beg, ++pos) {
+					if (!Rect.empty()) {
+						Rect.back().lower = min_element(Rect.back().lower, *Beg);
+						Rect.back().upper = max_element(Rect.back().upper, *Beg);
+					}
+					if (Line.size() % Step == 0) {
+						Rect.emplace_back(*Beg, *Beg);
 					}
 				}
 			}
@@ -67,29 +62,33 @@ namespace hmLib {
 
 				Rect.clear();
 				for(unsigned int i = 0; i<Line.size(); ++i) {
-					if(Rect.empty()) {
+					if (!Rect.empty()) {
 						Rect.back().lower = min_element(Rect.back().lower, Line[i]);
 						Rect.back().upper = max_element(Rect.back().upper, Line[i]);
-					} else {
-						Rect.back().lower = min_element(Rect.back().lower, Line[i]);
-						Rect.back().upper = max_element(Rect.back().upper, Line[i]);
-
-						if(i%Step == 0) {
-							Rect.emplace_back(Line[i], Line[i]);
-						}
+					}
+					if (i % Step == 0) {
+						Rect.emplace_back(Line[i], Line[i]);
 					}
 				}
 			}
-			void assign(line<T> Line_) {
-				Line = std::move(Line_);
-				reset_step(Step);
+			template<typename input_iterator>
+			void assign(input_iterator Beg, input_iterator End) {
+				clear();
+				push_back(Beg, End);
 			}
-			void assign(line<T> Line_, unsigned int Step_) {
+			template<typename input_iterator>
+			void assign(input_iterator Beg, input_iterator End, unsigned int Step_) {
+				clear();
+				reset_step(Step_)
+				push_back(Beg, End);
+			}
+			void assign(base_line&& Line_) {assign(std::move(Line_), Step);}
+			void assign(base_line&& Line_, unsigned int Step_) {
 				Line = std::move(Line_);
 				reset_step(Step_);
 			}
-			line<T> release() {
-				line<T> Ans;
+			base_line release() {
+				base_line Ans;
 				std::swap(Ans, Line);
 				Rect.clear();
 
@@ -113,10 +112,11 @@ namespace hmLib {
 			const_iterator find_cross(const segment<T>& s)const {
 				for(unsigned int i = 0; i<Rect.size(); ++i) {
 					if(is_cross(s, Rect[i])){
-						unsigned int End = std::min(Line.size()-1, i*Step+Step);
-						for(unsigned int j = i*Step; j<End; ++j) {
-							if(is_cross(s, segment<T>(Line[i], Line[i+1]))) {
-								return std::next(begin(), j);
+						unsigned int BegI = i * Step;
+						unsigned int EndI = std::min((i+1)*Step, Line.size() - 1);
+						for(; BegI<EndI; ++BegI) {
+							if(is_cross(s, segment<T>(Line[BegI], Line[BegI+1]))) {
+								return std::next(begin(), BegI);
 							}
 						}
 					}
@@ -126,10 +126,11 @@ namespace hmLib {
 			const_iterator find_cross(const rectangle<T>& r)const {
 				for(unsigned int i = 0; i<Rect.size(); ++i) {
 					if(is_cross(r, Rect[i])) {
-						unsigned int End = std::min(Line.size()-1, i*Step+Step);
-						for(unsigned int j = i*Step; j<End; ++j) {
-							if(is_cross(r, segment<T>(Line[i], Line[i+1]))) {
-								return std::next(begin(), j);
+						unsigned int BegI = i * Step;
+						unsigned int EndI = std::min((i + 1) * Step, Line.size() - 1);
+						for (; BegI < EndI; ++BegI) {
+							if (is_cross(r, segment<T>(Line[BegI], Line[BegI + 1]))) {
+								return std::next(begin(), BegI);
 							}
 						}
 					}
@@ -144,6 +145,7 @@ namespace hmLib {
 			}
 		};
 
+		/*
 		template<typename T, typename input_iterator, typename output_iterator>
 		output_iterator connect_closest_points(input_iterator beg, input_iterator end, T connect_distance, output_iterator out){
 			T lim = connect_distance*connect_distance;
@@ -183,7 +185,7 @@ namespace hmLib {
 			}
 
 			return out;
-		}
+		}*/
 	}
 }
 #
