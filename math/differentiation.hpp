549256e10f831d051de741e7c6dc12ef010433ad
Koichi Ito <hmito@outlook.com>
2019-09-25 18:20:49 -0700

    Fix other step negative issues in differentiation.

--- math/differentiation.hpp
+++ math/differentiation.hpp
@@ -7,7 +7,7 @@ namespace hmLib {
 		template<typename func_type, typename real_type>
 		void derivative(func_type fn, real_type x, real_type abserr, real_type relerr,
 			real_type& dx, real_type& dx2) {
-			auto hx = x * relerr + abserr;
+			auto hx = std::abs(x) * relerr + abserr;
 
 			auto v0 = fn(x);
 			auto vP = fn(x + hx);
@@ -20,7 +20,7 @@ namespace hmLib {
 		template<typename func_type, typename real_type>
 		void derivative(func_type fn, real_type x, real_type abserr, real_type relerr,
 			real_type& dx) {
-			auto hx = x * relerr + abserr;
+			auto hx = std::abs(x) * relerr + abserr;
 
 			auto vP = fn(x + hx);
 			auto vN = fn(x - hx);
@@ -55,8 +55,8 @@ namespace hmLib {
 		template<typename plane_func_type, typename real_type>
 		void plane_derivative(plane_func_type fn, real_type x, real_type y, real_type abserr, real_type relerr,
 			real_type& dx, real_type& dy) {
-			auto hx = x * relerr + abserr;
-			auto hy = y * relerr + abserr;
+			auto hx = std::abs(x) * relerr + abserr;
+			auto hy = std::abs(y) * relerr + abserr;
 
 			auto vP0 = fn(x + hx, y);
 			auto vN0 = fn(x - hx, y);
