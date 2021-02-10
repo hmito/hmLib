1e0706c1188b343ceb7ac055825d63f643f63efb
Koichi Ito <hmito@outlook.com>
2021-02-10 21:29:17 +0900

    Add cross_state_breaker.

--- odeint/breaker/state_breaker.hpp
+++ odeint/breaker/state_breaker.hpp
@@ -3,6 +3,7 @@
 #
 #include <array>
 #include "../utility.hpp"
+#include "../../geometry.hpp"
 namespace hmLib{
 	namespace odeint{
 		template<typename state_range_accessor_>
@@ -39,40 +40,6 @@ namespace hmLib{
 			unsigned int Cnt;
 			unsigned int Interval;
 			state_range_accessor Accessor;
-		private:
-			template<typename state>
-			bool is_cross_segment(const state& a1, const state& a2, const state& b1, const state& b2) {
-				if (a1[0] >= a2[0]) {
-					if ((a1[0] < b1[0] && a1[0] < b2[0]) || (a2[0] > b1[0] && a2[0] > b2[0])) {
-						return false;
-					}
-				}
-				else {
-					if ((a2[0] < b1[0] && a2[0] < b2[0]) || (a1[0] > b1[0] && a1[0] > b2[0])) {
-						return false;
-					}
-				}
-				if (a1[1] >= a2[1]) {
-					if ((a1[1] < b1[1] && a1[1] < b2[1]) || (a2[1] > b1[1] && a2[1] > b2[1])) {
-						return false;
-					}
-				}
-				else {
-					if ((a2[1] < b1[1] && a2[1] < b2[1]) || (a1[1] > b1[1] && a1[1] > b2[1])) {
-						return false;
-					}
-				}
-
-				if (((a1[0] - a2[0]) * (b1[1] - a1[1]) + (a1[1] - a2[1]) * (a1[0] - b1[0])) *
-					((a1[0] - a2[0]) * (b2[1] - a1[1]) + (a1[1] - a2[1]) * (a1[0] - b2[0])) > 0) {
-					return false;
-				}
-				if (((b1[0] - b2[0]) * (a1[1] - b1[1]) + (b1[1] - b2[1]) * (b1[0] - a1[0])) *
-					((b1[0] - b2[0]) * (a2[1] - b1[1]) + (b1[1] - b2[1]) * (b1[0] - a2[0])) > 0) {
-					return false;
-				}
-				return true;
-			}
 		public:
 			cross_state_breaker(unsigned int Interval_, state_range_accessor Accessor_) :Cnt(0), Interval(Interval_), Accessor(Accessor_){}
 			template<typename state_type, typename time_type>
@@ -81,14 +48,13 @@ namespace hmLib{
 				Cnt = 0;
 
 				auto Range = Accessor();
-				for (const auto& sx : Range) {
-					if (hmLib::odeint::distance_norm_inf(x, sx) < Tolerance) {
-						return true;
-					}
-				}
-				for (; Beg != End; ++Beg) {
-					if (is_cross_segment(x, y, p, Beg->second))return true;
-					p = Beg->second;
+				auto Last = std::next(std::begin(Range), std::distance(std::begin(), std::end()) - 1);
+				auto Prev = std::begin(Range);
+				auto s1 = hmLib::plane_geometry::make_semgnet(hmLib::plane_geometry::make_point(x), hmLib::plane_geometry::make_point(*Last));
+				for (auto Itr = std::next(Prev); Prev != Last; ++Itr, ++Prev) {
+					auto s2 = hmLib::plane_geometry::make_semgnet(hmLib::plane_geometry::make_point(*Itr), hmLib::plane_geometry::make_point(*Prev));
+
+					if(hmLib::plane_geometry::is_cross(s1, s2))return true;
 				}
 				return false;
 			}
