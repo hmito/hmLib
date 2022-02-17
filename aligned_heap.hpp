b1f9ace6c2bea12230e9c5b9fbe0539ba871b7c3
Koichi Ito <hmito@outlook.com>
2022-02-17 20:45:29 +0900

    aligned_heap is now ready.

--- aligned_heap.hpp
+++ aligned_heap.hpp
@@ -3,7 +3,7 @@
 #include<memory>
 namespace hmLib{
 	template<typename T>
-	struct aligned_heap{
+	struct aligned_heap final{
 		using pointer = T*;
 		using const_pointer = const T*;
 	private:
@@ -12,26 +12,50 @@ namespace hmLib{
 		T* End;
 	public:
 		aligned_heap():Ptr(nullptr),Beg(nullptr),End(nullptr){}
-		aligned_heap(aligned_heap<T>&& my){
-
+		explicit aligned_heap(std::size_t n):aligned_heap(){
+			allocate(n);
 		}
-		aligned_heap<T>& operator=(const aligned_heap<T>&)=delete;
+		aligned_heap(aligned_heap<T>&& other):aligned_heap(){
+			swap(other);
+		}
+		aligned_heap<T>& operator=(aligned_heap<T>&& other){
+			if(this != &other){
+				swap(other);
+			}			
+		}
 		aligned_heap(const aligned_heap<T>&)=delete;
 		aligned_heap<T>& operator=(const aligned_heap<T>&)=delete;
-
+		~aligned_heap(){deallocate();}
 	public:
 		void allocate(unsigned int n){
+			deallocate();
+
 			std::size_t bufsize = sizeof(T)*n + alignof(T);
 			Ptr = new unsigned char[bufsize];
-
-			Beg = static_cast<T*>(std::align(a, sizeof(T), Ptr, Ptr + bufsize));
+            
+            void* vPtr = Ptr;
+            std::align(alignof(T), sizeof(T), vPtr,bufsize);
+			Beg = static_cast<T*>(vPtr);
 			if(Beg)End = Beg + n;
 		}
 		void deallocate(){
-			if(Ptr)delete[] Ptr;
+			if(Ptr){
+				delete[] Ptr;
+				Ptr = nullptr;
+				Beg = nullptr;
+				End = nullptr;
+			}
 		}
+		void swap(aligned_heap<T>& other){
+			std::swap(Ptr, other.Ptr);
+			std::swap(Beg, other.Beg);
+			std::swap(End, other.End);
+		}
+		operator bool()const{return Beg!=nullptr;}
 		T* begin(){return Beg;}
 		T* end(){return End;}
+		std::size_t size()const{return End-Beg;}
 	};
+}
 #
 #endif
