ae0b2736b1c69c103c241c548896bcbe8f8b7c7b
Koichi Ito <hmito@outlook.com>
2022-02-19 22:01:18 +0900

    Add index_access_itr; index_access_itr was renamed as access_order_itr.

---
+++ iterators/access_iterator.hpp
@@ -0,0 +1,45 @@
+#ifndef HMLIB_ITERATORS_ACCESSITERATOR_INC
+#define HMLIB_ITERATORS_ACCESSITERATOR_INC 100
+#
+namespace hmLib{
+	namespace iterators{
+		namespace detail{
+			template<int n>
+			struct static_index_access{
+				template<typename iterator>
+				decltype(auto) operator()(iterator&&){
+					return (*itr)[n];
+				}
+			};
+			template<typename index_type>
+			struct index_access{
+			private:
+				index_type n;
+			public:
+				index_access(index_type n_):n(n_) {}
+				template<typename iterator>
+				decltype(auto) operator()(iterator&&){
+					return (*itr)[n];
+				}
+			};
+		}
+	}
+	template<int n, typename iterator>
+	decltype(auto) make_static_index_access_iterator(iterator Itr) {
+		return hmLib::make_transform_iterator(Itr, iterators::detail::static_index_access<n>());
+	}
+	template<int n, typename iterator>
+	decltype(auto) make_static_index_access_iterator(iterator Beg, iterator End) {
+		return hmLib::make_transform_range(Beg, End, iterators::detail::static_index_access<n>());
+	}
+	template<typename iterator, typename index_type>
+	decltype(auto) make_index_access_iterator(iterator Itr, index_type n) {
+		return hmLib::make_transform_iterator(Itr, iterators::detail::index_access(n));
+	}
+	template<int n, typename iterator>
+	decltype(auto) make_index_access_iterator(iterator Beg, iterator End) {
+		return hmLib::make_transform_range(Beg, End, iterators::detail::index_access(n));
+	}
+}
+#
+#endif
