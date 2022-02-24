54dc5d49224edbfe29711308d37879a16ef36e88
Koichi Ito <hmito@outlook.com>
2022-02-24 10:17:26 +0900

    Add make function in boundaries.

--- math/boundary.hpp
+++ math/boundary.hpp
@@ -5,7 +5,7 @@
 #include<cmath>
 #include"constants.hpp"
 namespace hmLib{
-	template<typename value_type_, typename fvalue_type_ = value_type>
+	template<typename value_type_, typename fvalue_type_ = value_type_>
 	struct lower_sine_boundary{
 		using value_type = value_type_;
 		using fvalue_type = fvalue_type_;
@@ -17,7 +17,7 @@ namespace hmLib{
 		fvalue_type outer_strength;
 	public:
 		template<typenma func>
-		lower_sine_boundary(func&& F, value_type lower_, value_type inner_width_, value_type outer_width_, fvalue_type outer_strength_=1)
+		lower_sine_boundary(func&& F, value_type lower_, value_type inner_width_, value_type outer_width_, fvalue_type outer_strength_)
 			: f(std::forward<func>(F))
 			, lower(lower_)
 			, inner_width(inner_width_)
@@ -25,7 +25,7 @@ namespace hmLib{
 			, outer_strength(outer_strength_){
 		}
 		template<typenma func>
-		lower_sine_boundary(func&& F, value_type lower_, value_type width_, fvalue_type outer_strength_=1)
+		lower_sine_boundary(func&& F, value_type lower_, value_type width_, fvalue_type outer_strength_)
 			: f(std::forward<func>(F))
 			, lower(lower_)
 			, inner_width(width_)
@@ -42,12 +42,10 @@ namespace hmLib{
 					return outer_strength;
 				}
 			}
-
 			return(f(x));
-
 		}
 	};
-	template<typename value_type_, typename fvalue_type_ = value_type>
+	template<typename value_type_, typename fvalue_type_ = value_type_>
 	struct upper_sine_boundary{
 		using value_type = value_type_;
 		using fvalue_type = fvalue_type_;
@@ -59,7 +57,7 @@ namespace hmLib{
 		fvalue_type outer_strength;
 	public:
 		template<typenma func>
-		upper_sine_boundary(func&& F, value_type upper_, value_type inner_width_, value_type outer_width_, fvalue_type outer_strength_=1)
+		upper_sine_boundary(func&& F, value_type upper_, value_type inner_width_, value_type outer_width_, fvalue_type outer_strength_)
 			: f(std::forward<func>(F))
 			, upper(upper_)
 			, inner_width(inner_width_)
@@ -67,7 +65,7 @@ namespace hmLib{
 			, outer_strength(outer_strength_){
 		}
 		template<typenma func>
-		upper_sine_boundary(func&& F, value_type upper_, value_type width_, fvalue_type outer_strength_=1)
+		upper_sine_boundary(func&& F, value_type upper_, value_type width_, fvalue_type outer_strength_)
 			: f(std::forward<func>(F))
 			, upper(upper_)
 			, inner_width(width_)
@@ -89,7 +87,7 @@ namespace hmLib{
 
 		}
 	};
-	template<typename value_type_, typename fvalue_type_ = value_type>
+	template<typename value_type_, typename fvalue_type_ = value_type_>
 	struct range_sine_boundary{
 		using value_type = value_type_;
 		using fvalue_type = fvalue_type_;
@@ -102,7 +100,7 @@ namespace hmLib{
 		fvalue_type outer_strength;
 	public:
 		template<typenma func>
-		range_sine_boundary(func&& F, value_type lower_, value_type upper_, value_type inner_width_, value_type outer_width_, fvalue_type outer_strength_=1)
+		range_sine_boundary(func&& F, value_type lower_, value_type upper_, value_type inner_width_, value_type outer_width_, fvalue_type outer_strength_)
 			: f(std::forward<func>(F))
 			, lower(lower_)
 			, upper(upper_)
@@ -111,7 +109,7 @@ namespace hmLib{
 			, outer_strength(outer_strength_){
 		}
 		template<typenma func>
-		range_sine_boundary(func&& F, value_type lower_, value_type upper_, value_type width_, fvalue_type outer_strength_=1)
+		range_sine_boundary(func&& F, value_type lower_, value_type upper_, value_type width_, fvalue_type outer_strength_)
 			: f(std::forward<func>(F))
 			, lower(lower_)
 			, upper(upper_)
@@ -143,6 +141,36 @@ namespace hmLib{
 			return(f(x));
 		}
 	};
+	template<typename func, typename value_type,typename fvalue_type_>
+	auto make_lower_sine_boundary(func&& F, value_type lower_, value_type width_, fvalue_type_ outer_strength_){
+		using fvalue_type = decltype(std::declval<func>()(std::declval<value_type>()));
+		return lower_sine_boundary<value_type,fvalue_type>(std::forward<func>(F),lower_,width_,outer_strength_);
+	}
+	template<typename func, typename value_type,typename fvalue_type_>
+	auto make_lower_sine_boundary(func&& F, value_type lower_,value_type inner_width_, value_type outer_width_, fvalue_type_ outer_strength_){
+		using fvalue_type = decltype(std::declval<func>()(std::declval<value_type>()));
+		return lower_sine_boundary<value_type,fvalue_type>(std::forward<func>(F),lower_,inner_width_,outer_width_,outer_strength_);
+	}
+	template<typename func, typename value_type,typename fvalue_type_>
+	auto make_upper_sine_boundary(func&& F, value_type upper_, value_type width_, fvalue_type_ outer_strength_){
+		using fvalue_type = decltype(std::declval<func>()(std::declval<value_type>()));
+		return upper_sine_boundary<value_type,fvalue_type>(std::forward<func>(F),upper_,width_,outer_strength_);
+	}
+	template<typename func, typename value_type,typename fvalue_type_>
+	auto make_upper_sine_boundary(func&& F, value_type upper_,value_type inner_width_, value_type outer_width_, fvalue_type_ outer_strength_){
+		using fvalue_type = decltype(std::declval<func>()(std::declval<value_type>()));
+		return upper_sine_boundary<value_type,fvalue_type>(std::forward<func>(F),upper_,inner_width_,outer_width_,outer_strength_);
+	}
+	template<typename func, typename value_type,typename fvalue_type_>
+	auto make_range_sine_boundary(func&& F, value_type lower_,value_type upper_, value_type width_, fvalue_type_ outer_strength_){
+		using fvalue_type = decltype(std::declval<func>()(std::declval<value_type>()));
+		return range_sine_boundary<value_type,fvalue_type>(std::forward<func>(F),lower_,upper_,width_,outer_strength_);
+	}
+	template<typename func, typename value_type,typename fvalue_type_>
+	auto make_range_sine_boundary(func&& F, value_type lower_,value_type upper_,value_type inner_width_, value_type outer_width_, fvalue_type_ outer_strength_){
+		using fvalue_type = decltype(std::declval<func>()(std::declval<value_type>()));
+		return range_sine_boundary<value_type,fvalue_type>(std::forward<func>(F),lower_,upper_,inner_width_,outer_width_,outer_strength_);
+	}
 }
 #
 #endif
