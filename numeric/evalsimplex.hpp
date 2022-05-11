30982e605161a36826bbd720adc239ce67e72cad
Koichi Ito <hmito@outlook.com>
2022-05-11 20:48:53 +0900

    Add evalsimplex; TODO: initialize simplex for value_type...

---
+++ numeric/evalsimplex.hpp
@@ -0,0 +1,92 @@
+#ifndef HMLIB_NUMERIC_SIMPLEX_INC
+#define HMLIB_NUMERIC_SIMPLEX_INC 100
+#
+#include<cmath>
+#include<iterator>
+#include<vector>
+#include"evalpair.hpp"
+namespace hmLib{
+	namespace numeric{
+		template<typename value_type, typename evalue_type>
+		struct evalsimplex{
+			using pair = evalpair<value_type,evalue_type>
+		private:
+			using container = std::vector<vertex>;
+		public:
+			using iterator = decltype(std::declval<container>().begin());
+			using const_iterator = decltype(std::declval<container>().cbegin());
+			using reference = vertex&;
+			using const_reference = const vertex&;
+		private:
+			std::vector<pair> Simplex;
+		public:
+			evalsimplex() = default;
+			template<typename fn, typename value_type, typename error_type>
+			evalsimplex(fn Fn, value_type inival, error_type rel_value, error_type abs_value) {
+				std::size_t size = std::distance(beg,end);
+				//initialize vertex
+				Simplex.reserve(size+1);
+				Simplex.emplace_back(Fn, inival);
+				//Simplex.assign(size + 1, vertex(ini, Fn(ini)));
+				for (std::size_t i = 1; i < Simplex.size(); ++i) {
+					auto val = inival;
+					auto itr = std::next(Simplex[i].value().begin(), i - 1);
+					(*itr) += abs_value + rel_value * (*itr);
+					Simplex[i].fvalue() = Fn(Simplex[i].value());
+				}
+			}
+			iterator begin() { return Simplex.begin(); }
+			iterator end() { return Simplex.end(); }
+			const_iterator begin()const { return Simplex.begin(); }
+			const_iterator end()const { return Simplex.end(); }
+			const_iterator cbegin()const { return Simplex.cbegin(); }
+			const_iterator cend()const { return Simplex.cend(); }
+			reference at(unsigned int n) { return Simplex.at(n); }
+			const_reference at(unsigned int n)const { return Simplex.at(n); }
+			reference operator[](unsigned int n) { return Simplex[n]; }
+			const_reference operator[](unsigned int n)const { return Simplex[n]; }
+			const_iterator minima()const { return std::min_element(Simplex.begin(),Simplex.end());}
+		};
+		template<typename error_type_>
+		struct simplex_precision_breaker{
+			//following functions should be callable;
+			// state_type::begin() : return begin iterator of the node array of the simplex
+			// state_type::end() : return end iterator of the node array of simplex
+			//  each node also should have begin/end for access value on each axis. 
+			using error_type = error_type_;
+		public:
+			simplex_precision_breaker() = delete;
+			explicit simplex_precision_breaker(error_type relative_error_)
+				: relerr(relative_error_)
+				, abserr(relative_error_/4){
+			}
+			simplex_precision_breaker(error_type relative_error_, error_type absolute_error_)
+				: relerr(relative_error_)
+				, abserr(absolute_error_){
+			}
+			template<typename value_type, typename evalue_type>
+			bool operator()(const evalsimplex<value_type,evalue_type>& x,step_type)const{
+				unsigned int size = std::distance(x.begin(), x.end()) - 1;
+				auto minima = x.minima();
+				for (unsigned int i = 0; i < size; ++i) {
+					auto nitr = x.begin(); 
+					auto lower = (nitr->v)[i];
+					auto upper = (nitr->v)[i];
+					++nitr;
+					for(;nitr != x.end(); ++nitr){
+						lower = std::min(lower,(nitr->v)[i]);
+						upper = std::max(upper,(nitr->v)[i]);
+					}
+
+					if(std::abs(minima.v[i] - (upper + lower) / 2) + (upper - lower) / 2 > (relerr * std::abs(minima.v[i]) + abserr / 4) * 2){
+						return false;
+					}
+				}
+				return true;
+			}
+		private:
+			error_type relerr;
+			error_type abserr;
+		};
+	}
+}
