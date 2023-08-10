786dedb5c42f0dbab2b26de1fb1fba522153f021
Koichi Ito <hmito@outlook.com>
2023-08-10 22:57:06 +0900

    Add old hill_climb cod but not stepper style.

--- numeric/hill_climb_minima.hpp
+++ numeric/hill_climb_minima.hpp
@@ -1,19 +1,15 @@
-#ifndef HMLIB_MINIMA_INC
-#define HMLIB_MINIMA_INC 100
+#ifndef HMLIB_NUMERIC_HILLCLIMBMINIMA_INC
+#define HMLIB_NUMERIC_HILLCLIMBMINIMA_INC 100
 #
 #include<random>
-#include<utility>
-#include"math/constants.hpp"
-#include"minima/golden_section_minima.hpp"
-#include"minima/brent_minima.hpp"
-#include"minima/nelder_mead_minima.hpp"
+#include"evalue.hpp"
 namespace hmLib {
-	namespace minima {
+	namespace numeric {
 		//hill climing search
-		template<typename evaluate, typename state, typename mutate, typename urbg, typename state_breaker>
-		auto hill_climbing_minima(evaluate&& Evaluate, state& State, mutate&& Mutate, state_breaker&& Breaker, urbg&& URBG)->decltype(Evaluate(State)){
+		template<typename evaluate, typename state_type, typename mutate, typename urbg, typename state_breaker>
+		auto hill_climbing_minima(evaluate&& Evaluate, state_type& State, mutate&& Mutate, state_breaker&& Breaker, urbg&& URBG)->decltype(Evaluate(State)){
 			auto Eval = Evaluate(State);
-			state NState = State;
+			state_type NState = State;
 
 			while(!Breaker(State, Eval)){
 				Mutate(NState, URBG);
@@ -31,10 +27,10 @@ namespace hmLib {
 		}
 
 		//simulated annealing search
-		template<typename evaluate, typename state, typename mutate, typename temp, typename urbg, typename state_breaker>
-		auto simulated_annealing_minima(evaluate&& Evaluate, state& State, mutate&& Mutate, temp&& Temp, state_breaker&& Breaker, unsigned int StepNum, urbg&& URBG)->decltype(Evaluate(State)){
+		template<typename evaluate, typename state_type, typename mutate, typename temp, typename urbg, typename state_breaker>
+		auto simulated_annealing_minima(evaluate&& Evaluate, state_type& State, mutate&& Mutate, temp&& Temp, state_breaker&& Breaker, unsigned int StepNum, urbg&& URBG)->decltype(Evaluate(State)){
 			auto Eval = Evaluate(State);
-			state NState = State;
+			state_type NState = State;
 
 			unsigned int StepCnt = 0;
 			while(!Breaker(State, Eval)){
@@ -49,25 +45,6 @@ namespace hmLib {
 				}
 
 				++StepCnt;
-			}
-
-			return Eval;
-		}
-
-		//brute-forece search
-		template<typename evaluate, typename state, typename state_stepper>
-		auto brute_force_minima(evaluate&& Evaluate, state& State, state_stepper&& StateStepper)->decltype(Evaluate(State)){
-			StateStepper.initialize(State);
-
-			auto Eval = Evaluate(State);
-			state NState = State;
-
-			while(!StateStepper(NState)){
-				auto NEval = Evaluate(NState);
-				if(Eval > NEval){
-					Eval = NEval;
-					State = NState;
-				}
 			}
 
 			return Eval;
